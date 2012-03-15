9177753c2874fb4825de5e5c873d209809b8cdc1
Alessio Di Sandro <alessio.disandro@gmail.com>
2012-03-15 00:22:13 +0000

    Basic mid (wip): * Test hard-coded statechart creation.

--- edu.toronto.cs.se.mmtf.mid.diagram/src/edu/toronto/cs/se/mmtf/mid/diagram/edit/commands/ModelReferenceCreateModelCommand.java
+++ edu.toronto.cs.se.mmtf.mid.diagram/src/edu/toronto/cs/se/mmtf/mid/diagram/edit/commands/ModelReferenceCreateModelCommand.java
@@ -19,10 +19,18 @@
 package edu.toronto.cs.se.mmtf.mid.diagram.edit.commands;
 
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWizard;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.wizards.IWizardDescriptor;
 
 import edu.toronto.cs.se.mmtf.mid.MidFactory;
 import edu.toronto.cs.se.mmtf.mid.ModelReference;
@@ -31,12 +39,42 @@ import edu.toronto.cs.se.mmtf.mid.MultiModel;
 
 public class ModelReferenceCreateModelCommand extends ModelReference2CreateCommand {
 
+	private URI createModel() {
+
+		URI modelUri = null;
+		//TODO show dialog to ask which model should be created (populated from repository)
+			//TODO get all metamodels, all wizards for each metamodel
+		//TODO get selected wizardID, launch wizard
+		//TODO find a way to get the uri of the generated model/diagrams
+
+		return modelUri;
+	}
+
 	public ModelReferenceCreateModelCommand(CreateElementRequest req) {
 
 		super(req);
 	}
 
 	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+
+		IWizardDescriptor descriptor = PlatformUI
+				.getWorkbench()
+				.getNewWizardRegistry()
+				.findWizard(
+						"edu.toronto.cs.se.mmtf.xt.statechart.diagram.part.StatechartCreationWizardID");
+		try {
+			if (descriptor != null) {
+				IWorkbenchWizard wizard = descriptor.createWizard();
+				wizard.init(PlatformUI.getWorkbench(), new StructuredSelection());
+				Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+				WizardDialog dialog = new WizardDialog(shell, wizard);
+				dialog.setTitle(wizard.getWindowTitle());
+				dialog.open();
+				// check return
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
 
 		ModelReference newElement = MidFactory.eINSTANCE.createModelReference();
 		newElement.setOrigin(ModelReferenceOrigin.CREATED);
