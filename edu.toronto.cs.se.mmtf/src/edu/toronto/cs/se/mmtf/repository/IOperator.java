74fd600bdef120df312edb6beed6cefe0722cc14
Alessio Di Sandro <alessio.disandro@gmail.com>
2012-03-27 22:48:45 +0000

    Basic mid (wip): * Add basic operator support.

--- edu.toronto.cs.se.mmtf/src/edu/toronto/cs/se/mmtf/repository/IOperator.java
+++ edu.toronto.cs.se.mmtf/src/edu/toronto/cs/se/mmtf/repository/IOperator.java
@@ -1,5 +1,11 @@
 package edu.toronto.cs.se.mmtf.repository;
 
+import org.eclipse.emf.ecore.EObject;
+
+import edu.toronto.cs.se.mmtf.MMTFException;
+
 public interface IOperator {
+
+	public EObject[] invoke(EObject[] parameters) throws MMTFException;
 
 }
