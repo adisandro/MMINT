569488877775af1e96926efecf0103f59578d2c2
Alessio Di Sandro <alessio.disandro@gmail.com>
2012-07-16 21:23:46 +0000

    Test driver: * Created stub for ExperimentDriver operator.

---
+++ edu.toronto.cs.se.modelepedia.operator/src/edu/toronto/cs/se/modelepedia/operator/ExperimentDriver.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2012 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay, Vivien Suen.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
+package edu.toronto.cs.se.modelepedia.operator;
+
+import org.eclipse.emf.common.util.EList;
+
+import edu.toronto.cs.se.mmtf.mid.Model;
+import edu.toronto.cs.se.mmtf.mid.operator.impl.OperatorExecutableImpl;
+
+public class ExperimentDriver extends OperatorExecutableImpl {
+
+	@Override
+	public EList<Model> execute(EList<Model> actualParameters) throws Exception {
+
+		return null;
+	}
+
+}
