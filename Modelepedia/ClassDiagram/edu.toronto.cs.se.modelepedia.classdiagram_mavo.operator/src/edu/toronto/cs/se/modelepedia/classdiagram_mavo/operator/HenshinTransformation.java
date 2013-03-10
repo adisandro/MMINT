90a7ae1fc0219a8fd91c3add76cf5e476a0fcc06
Alessio Di Sandro <alessio.disandro@gmail.com>
2013-03-10 20:55:08 +0000

    MODELS13: * Added proof of concept to identify and propagate may annotations.

--- Modelepedia/ClassDiagram/edu.toronto.cs.se.modelepedia.classdiagram_mavo.operator/src/edu/toronto/cs/se/modelepedia/classdiagram_mavo/operator/HenshinTransformation.java
+++ Modelepedia/ClassDiagram/edu.toronto.cs.se.modelepedia.classdiagram_mavo.operator/src/edu/toronto/cs/se/modelepedia/classdiagram_mavo/operator/HenshinTransformation.java
@@ -11,10 +11,14 @@
  */
 package edu.toronto.cs.se.modelepedia.classdiagram_mavo.operator;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.henshin.interpreter.EGraph;
 import org.eclipse.emf.henshin.interpreter.Engine;
 import org.eclipse.emf.henshin.interpreter.Match;
@@ -23,9 +27,11 @@ import org.eclipse.emf.henshin.interpreter.impl.EGraphImpl;
 import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
 import org.eclipse.emf.henshin.interpreter.impl.RuleApplicationImpl;
 import org.eclipse.emf.henshin.model.Module;
+import org.eclipse.emf.henshin.model.Node;
 import org.eclipse.emf.henshin.model.Rule;
 import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
 
+import edu.toronto.cs.se.mmtf.mavo.MAVOElement;
 import edu.toronto.cs.se.mmtf.mid.Model;
 import edu.toronto.cs.se.mmtf.mid.operator.impl.OperatorExecutableImpl;
 import edu.toronto.cs.se.mmtf.mid.trait.MultiModelOperatorUtils;
@@ -58,28 +64,45 @@ public class HenshinTransformation extends OperatorExecutableImpl {
 		);
 		readProperties(inputProperties);
 
+		// do transformation
 		String fullUri = MultiModelRegistry.prependWorkspaceToUri(MultiModelRegistry.replaceLastSegmentInUri(cdModel.getUri(), ""));
 		HenshinResourceSet resourceSet = new HenshinResourceSet(fullUri);
 		Module module = resourceSet.getModule(transformationModule, false);
 		Engine engine = new EngineImpl();
 		EGraph graph = new EGraphImpl(resourceSet.getResource(MultiModelRegistry.getLastSegmentFromUri(cdModel.getUri())));
-
 		Rule rule = (Rule) module.getUnit(transformationUnit);
 		RuleApplication application = new RuleApplicationImpl(engine);
 		application.setRule(rule);
 		application.setEGraph(graph);
 		for (Match match : engine.findMatches(rule, graph, null)) {
+			Set<MAVOElement> mayContext = new HashSet<MAVOElement>();
+			boolean propagateMay = false;
+			for (EObject node : match.getNodeTargets()) {
+				if (!(node instanceof MAVOElement)) {
+					continue;
+				}
+				mayContext.add((MAVOElement) node);
+				propagateMay |= ((MAVOElement) node).isMay();
+			}
 			application.setCompleteMatch(match);
 			application.execute(null);
+			Match resultMatch = application.getResultMatch();
+			for (EObject resultNode : resultMatch.getNodeTargets()) {
+				if (!(resultNode instanceof MAVOElement)) {
+					continue;
+				}
+				// created elements
+				if (!mayContext.contains((MAVOElement) resultNode)) {
+					((MAVOElement) resultNode).setMay(propagateMay);
+				}
+			}
 		}
-
-		//TODO MMTF: inspect match, look for may and propagate
 
 		resourceSet.saveEObject(
 			graph.getRoots().get(0),
 			MultiModelRegistry.getLastSegmentFromUri(MultiModelRegistry.addFileNameSuffixInUri(cdModel.getUri(), TRANSFORMED_MODEL_SUFFIX))
 		);
-		//TODO MMTF: create model for result, add to mid
+		//TODO MMTF: create model for result, add to mid?
 		EList<Model> result = new BasicEList<Model>();
 
 		return result;
