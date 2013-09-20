08bdf0c880df004b437f48e811b7564cb0017a0d
Alessio Di Sandro <alessio.disandro@gmail.com>
2013-09-20 16:45:52 +0000

    1.1.1: * Updated all copyrights.

--- Modelepedia/IStar/edu.toronto.cs.se.modelepedia.istar_mavo.operator/src/edu/toronto/cs/se/modelepedia/istar_mavo/operator/RE13.java
+++ Modelepedia/IStar/edu.toronto.cs.se.modelepedia.istar_mavo.operator/src/edu/toronto/cs/se/modelepedia/istar_mavo/operator/RE13.java
@@ -1,6 +1,6 @@
-/*
- * Copyright (c) 2012 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
- * Rick Salay, Vivien Suen.
+/**
+ * Copyright (c) 2013 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
--- Modelepedia/LabeledGraph/edu.toronto.cs.se.modelepedia.labeledgraph/src-gen/edu/toronto/cs/se/modelepedia/labeledgraph/impl/LabeledGraphFactoryImpl.java
+++ Modelepedia/LabeledGraph/edu.toronto.cs.se.modelepedia.labeledgraph/src-gen/edu/toronto/cs/se/modelepedia/labeledgraph/impl/LabeledGraphFactoryImpl.java
@@ -1,13 +1,13 @@
 /**
- * Copyright (c) 2012 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
- * Rick Salay, Vivien Suen.
+ * Copyright (c) 2013 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *    Alessio Di Sandro, Vivien Suen - Implementation.
+ *    Alessio Di Sandro - Implementation.
  */
 package edu.toronto.cs.se.modelepedia.labeledgraph.impl;
 
