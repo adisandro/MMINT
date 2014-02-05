4e4f78b6ede4b3fcf7598928ea5fe52e16b8c154
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-02-04 23:40:38 -0500

    tests: Create stub project for first test

---
+++ Tests/edu.toronto.cs.se.modelepedia.tests/src/edu/toronto/cs/se/modelepedia/tests/KleisliTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2012-2014 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
+package edu.toronto.cs.se.modelepedia.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KleisliTest {
+
+	@Test
+	public void test() {
+		//TODO MMTF[TESTS] open type mid, then:
+		//use similar code in RunOperatorListener to run a mid diagram command?
+		//or simply invoke the api to avoid simulating clicks?
+		fail("TODO");
+	}
+
+}
