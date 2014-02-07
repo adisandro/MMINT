45d344e4cd10d9dd66d4a8ef4e87b0a006d08bf2
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-02-06 23:38:23 -0500

    tests: Make kleisli test a plugin
    * Convert to plugin
    * Add test metamodels
    * Add first draft of test

--- Tests/edu.toronto.cs.se.modelepedia.tests/src/edu/toronto/cs/se/modelepedia/tests/KleisliTest.java
+++ Tests/edu.toronto.cs.se.modelepedia.tests/src/edu/toronto/cs/se/modelepedia/tests/KleisliTest.java
@@ -15,13 +15,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.toronto.cs.se.mmtf.MMTF;
+import edu.toronto.cs.se.mmtf.MultiModelTypeRegistry;
+import edu.toronto.cs.se.mmtf.mid.MidFactory;
+import edu.toronto.cs.se.mmtf.mid.Model;
+import edu.toronto.cs.se.mmtf.mid.ModelEndpoint;
+import edu.toronto.cs.se.mmtf.mid.ModelOrigin;
+import edu.toronto.cs.se.mmtf.mid.MultiModel;
+import edu.toronto.cs.se.mmtf.mid.relationship.ModelRel;
+
 public class KleisliTest {
 
+	private final static String KLEISLI_MODELREL_URI = "http://se.cs.toronto.edu/modelepedia/KleisliModelRel";
+
 	@Test
-	public void test() {
-		//TODO MMTF[TESTS] open type mid, then:
-		//use similar code in RunOperatorListener to run a mid diagram command?
-		//or simply invoke the api to avoid simulating clicks?
+	public void test() throws Exception {
+
+		//TODO MMTF[TESTS] MMTF has to run before this, with dynamic types from previous run discarded
+		Model rootModelType = MultiModelTypeRegistry.getType(MMTF.ROOT_MODEL_URI);
+		Model bankModelType = rootModelType.createSubtype("Bank", null, null, true);
+		//TODO MMTF[TESTS] copy Bank.ecore metamodel from model folder
+		Model libraryModelType = rootModelType.createSubtype("Library", null, null, true);
+		//TODO MMTF[TESTS] copy Library.ecore metamodel from model folder
+		ModelRel kRootModelRelType = MultiModelTypeRegistry.getType(KLEISLI_MODELREL_URI);
+		ModelRel kModelRelType = kRootModelRelType.createSubtype("Bank-->Library", false, null, null);
+		ModelEndpoint kRootModelTypeEndpoint = kRootModelRelType.getModelEndpoints().get(0);
+		//TODO MMTF[TESTS] copy Bank-Bank.ecore metamodel from model folder
+		kRootModelTypeEndpoint.createSubtypeAndReference(null, "Bank", bankModelType, false, kModelRelType);
+		//TODO MMTF[TESTS] here we need MMTF.isInitialized() to return false
+		kRootModelTypeEndpoint.createSubtypeAndReference(null, "Library", libraryModelType, false, kModelRelType);
+		//TODO MMTF[TESTS] create model element types, ocl queries and link types
+		MultiModel instanceMID = MidFactory.eINSTANCE.createMultiModel();
+		//TODO MMTF[TESTS] should try to mimic things in gmf command or this is all?
+		bankModelType.createInstanceAndEditor("test/bank.xmi", ModelOrigin.CREATED, instanceMID);
+		//TODO MMTF[TESTS] create other instances
 		fail("TODO");
 	}
 
