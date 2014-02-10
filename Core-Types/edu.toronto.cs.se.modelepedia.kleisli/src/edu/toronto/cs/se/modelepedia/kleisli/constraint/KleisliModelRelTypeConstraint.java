eddc1f96e836cb2f4538b275bb781a55da0616e3
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-02-10 14:02:26 -0500

    kleisli: Add homomorphism constraint to kleisli rels

---
+++ Core-Types/edu.toronto.cs.se.modelepedia.kleisli/src/edu/toronto/cs/se/modelepedia/kleisli/constraint/KleisliModelRelTypeConstraint.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2012-2014 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
+package edu.toronto.cs.se.modelepedia.kleisli.constraint;
+
+import edu.toronto.cs.se.mmtf.mid.Model;
+import edu.toronto.cs.se.mmtf.mid.constraint.JavaModelConstraint;
+import edu.toronto.cs.se.mmtf.mid.relationship.LinkReference;
+import edu.toronto.cs.se.mmtf.mid.relationship.ModelElementReference;
+import edu.toronto.cs.se.mmtf.reasoning.Z3SMTUtils.MAVOTruthValue;
+import edu.toronto.cs.se.mmtf.transformation.ModelRelTypeTransformationConstraint;
+import edu.toronto.cs.se.modelepedia.kleisli.KleisliModelRel;
+
+public class KleisliModelRelTypeConstraint extends JavaModelConstraint {
+
+	public KleisliModelRelTypeConstraint(Model model) {
+
+		super(model);
+	}
+
+	@Override
+	public MAVOTruthValue validate() {
+
+		KleisliModelRel kModelRelType = (KleisliModelRel) model;
+		if (new ModelRelTypeTransformationConstraint(kModelRelType).validate() != MAVOTruthValue.TRUE) {
+			return MAVOTruthValue.FALSE;
+		}
+		// must be homomorphism
+		for (ModelElementReference modelElemTypeRef : kModelRelType.getModelEndpointRefs().get(0).getModelElemRefs()) {
+			if (
+				modelElemTypeRef.getModelElemEndpointRefs().size() != 1 ||
+				((LinkReference) modelElemTypeRef.getModelElemEndpointRefs().get(0).eContainer()).getModelElemEndpointRefs().size() != 2
+			) {
+				return MAVOTruthValue.FALSE;
+			}
+		}
+		for (ModelElementReference modelElemTypeRef : kModelRelType.getModelEndpointRefs().get(1).getModelElemRefs()) {
+			if (modelElemTypeRef.getModelElemEndpointRefs().size() > 1) {
+				return MAVOTruthValue.FALSE;
+			}
+		}
+
+		return MAVOTruthValue.TRUE;
+	}
+
+}
