c448b23e9181c689d332451530fc4bcdc93e58c1
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-05-05 12:19:20 -0400

    reasoning: Decouple reasoning engines from core mmint (9)
     * Add ocl project

---
+++ Core-Types/edu.toronto.cs.se.modelepedia.ocl/src/edu/toronto/cs/se/modelepedia/ocl/reasoning/OCLReasoningEngine.java
@@ -0,0 +1,127 @@
+package edu.toronto.cs.se.modelepedia.ocl.reasoning;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.ocl.examples.pivot.ExpressionInOCL;
+import org.eclipse.ocl.examples.pivot.OCL;
+import org.eclipse.ocl.examples.pivot.Type;
+import org.eclipse.ocl.examples.pivot.ecore.Ecore2Pivot;
+import org.eclipse.ocl.examples.pivot.helper.OCLHelper;
+import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
+
+import edu.toronto.cs.se.mmint.MMINTException;
+import edu.toronto.cs.se.mmint.mid.ExtendibleElementConstraint;
+import edu.toronto.cs.se.mmint.mid.MidLevel;
+import edu.toronto.cs.se.mmint.mid.Model;
+import edu.toronto.cs.se.mmint.mid.constraint.MultiModelConstraintChecker;
+import edu.toronto.cs.se.mmint.mid.constraint.MultiModelConstraintChecker.MAVOTruthValue;
+import edu.toronto.cs.se.mmint.mid.relationship.ModelEndpointReference;
+import edu.toronto.cs.se.mmint.mid.relationship.ModelRel;
+import edu.toronto.cs.se.mmint.reasoning.ReasoningEngine;
+
+public class OCLReasoningEngine implements ReasoningEngine {
+
+	private final static String OCL_MODELENDPOINT_VARIABLE = "$ENDPOINT_";
+	private final static char OCL_VARIABLE_SEPARATOR = '.';
+
+	private void initOCL(OCLHelper helper, EObject modelObj) {
+
+		//TODO MMINT: workaround for bug #375485
+//		helper.setInstanceContext(modelObj);
+		MetaModelManager metaModelManager = helper.getOCL().getMetaModelManager();
+		EClass modelObjClass = modelObj.eClass();
+		Resource resource = modelObjClass.eResource();
+		Ecore2Pivot ecore2Pivot = Ecore2Pivot.getAdapter(resource, metaModelManager);
+		Type pivotType = ecore2Pivot.getCreated(Type.class, modelObjClass);
+		helper.setContext(pivotType);
+	}
+
+	private EObject getConstraintContext(Model model, String oclConstraint, MidLevel constraintLevel) throws MMINTException {
+
+		//TODO MMINT[CONSTRAINT] find language to express more complex contraints on model rels
+		boolean isInstancesLevel = MultiModelConstraintChecker.isInstancesLevel(model);
+		EObject modelObj = null;
+		if (model instanceof ModelRel && oclConstraint.startsWith(OCL_MODELENDPOINT_VARIABLE)) {
+			String modelEndpointConstraintName = oclConstraint.substring(OCL_MODELENDPOINT_VARIABLE.length(), oclConstraint.indexOf(OCL_VARIABLE_SEPARATOR));
+			for (ModelEndpointReference modelEndpointRef : ((ModelRel) model).getModelEndpointRefs()) {
+				//TODO MMINT[ENDPOINT] consider overridden endpoints here
+				String modelEndpointName = (isInstancesLevel && constraintLevel != MidLevel.INSTANCES) ?
+					modelEndpointRef.getObject().getMetatype().getName() :
+					modelEndpointRef.getObject().getName();
+				if (modelEndpointConstraintName.equals(modelEndpointName)) {
+					modelObj = (isInstancesLevel) ?
+						modelEndpointRef.getObject().getTarget().getEMFInstanceRoot() :
+						modelEndpointRef.getObject().getTarget().getEMFTypeRoot();
+					break;
+				}
+			}
+			if (modelObj == null) {
+				throw new MMINTException("Can't find model endpoint " + modelEndpointConstraintName + " used in model relationship constraint");
+			}
+		}
+		else {
+			modelObj = (isInstancesLevel) ?
+				model.getEMFInstanceRoot() :
+				model.getEMFTypeRoot();
+		}
+
+		return modelObj;
+	}
+
+	@Override
+	public MAVOTruthValue checkConstraint(Model model, ExtendibleElementConstraint constraint, MidLevel constraintLevel) {
+
+		String oclConstraint = constraint.getImplementation();
+		try {
+			EObject modelObj = getConstraintContext(model, oclConstraint, constraintLevel);
+			if (model instanceof ModelRel && oclConstraint.startsWith(OCL_MODELENDPOINT_VARIABLE)) {
+				oclConstraint = oclConstraint.substring(oclConstraint.indexOf(OCL_VARIABLE_SEPARATOR) + 1, oclConstraint.length());
+			}
+			return checkConstraint(modelObj, oclConstraint);
+		}
+		catch (MMINTException e) {
+			MMINTException.print(MMINTException.Type.ERROR, "Can't get context for OCL constraint, evaluating to false", e);
+			return MAVOTruthValue.FALSE;
+		}
+	}
+
+	@Override
+	public boolean checkConstraintConsistency(Model modelType, String constraint) {
+
+		return true;
+	}
+
+	public MAVOTruthValue checkConstraint(EObject modelObj, String oclConstraint) {
+
+		OCL ocl = OCL.newInstance();
+		OCLHelper helper = ocl.createOCLHelper();
+		initOCL(helper, modelObj);
+
+		try {
+			ExpressionInOCL expression = helper.createInvariant(oclConstraint);
+			return (ocl.check(modelObj, expression)) ? MAVOTruthValue.TRUE : MAVOTruthValue.FALSE;
+		}
+		catch (Exception e) {
+			MMINTException.print(MMINTException.Type.ERROR, "OCL constraint error, evaluating to false: " + oclConstraint, e);
+			return MAVOTruthValue.FALSE;
+		}
+	}
+
+	public Object deriveUsingConstraint(EObject modelObj, String oclConstraint) {
+
+		OCL ocl = OCL.newInstance();
+		OCLHelper helper = ocl.createOCLHelper();
+		initOCL(helper, modelObj);
+
+		try {
+			ExpressionInOCL expression = helper.createQuery(oclConstraint);
+			return ocl.evaluate(modelObj, expression);
+		}
+		catch (Exception e) {
+			MMINTException.print(MMINTException.Type.ERROR, "OCL derivation error: " + oclConstraint, e);
+			return null;
+		}
+	}
+
+}
