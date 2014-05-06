6858ed3f8c5777e5da4472bfc7b78036a7a2649d
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-05-06 18:32:06 -0400

    reasoning: Decouple reasoning engines from core mmint (14)
     * Final commit, all reviewed, remove todos: yay!

--- Core/edu.toronto.cs.se.mmint/src/edu/toronto/cs/se/mmint/reasoning/IReasoningEngine.java
+++ Core/edu.toronto.cs.se.mmint/src/edu/toronto/cs/se/mmint/reasoning/IReasoningEngine.java
@@ -18,15 +18,6 @@ import edu.toronto.cs.se.mmint.mid.constraint.MultiModelConstraintChecker.MAVOTr
 
 public interface IReasoningEngine {
 
-	/*TODO:
-	 * 1)- Define interface to check constraint
-	 * 2)- Define extension point to plug in reasoners
-	 * 3)- Read extension point in MMINT and instantiate reasoners
-	 * 4)- Dispatch to the appropriate reasoner in MultiModelConstraintChecker.checkConstraint()
-	 * 5)- Remove ExtendibleElementConstraintLanguage enum and use engine id
-	 * 6)- Isolate all reasoners (OCL, JAVA, SMTLIB, EMFtoCSP) in their own projects
-	 * 7) Review constraint input
-	 */
 	public MAVOTruthValue checkConstraint(Model model, ExtendibleElementConstraint constraint, MidLevel constraintLevel);
 
 	public boolean checkConstraintConsistency(Model modelType, String constraint);
