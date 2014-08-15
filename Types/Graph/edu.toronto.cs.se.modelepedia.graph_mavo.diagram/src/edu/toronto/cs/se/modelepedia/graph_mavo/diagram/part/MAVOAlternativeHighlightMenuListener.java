a57df605b19b7f30fcaa700e2306a9a1c6f50ab7
Naama Ben-David <naamabd14@gmail.com>
2014-08-15 11:23:05 -0400

    mu-mmint: adding highlight alternative menu item and 'add element to alternative' from decision tree

--- Types/Graph/edu.toronto.cs.se.modelepedia.graph_mavo.diagram/src/edu/toronto/cs/se/modelepedia/graph_mavo/diagram/part/MAVOAlternativeHighlightMenuListener.java
+++ Types/Graph/edu.toronto.cs.se.modelepedia.graph_mavo.diagram/src/edu/toronto/cs/se/modelepedia/graph_mavo/diagram/part/MAVOAlternativeHighlightMenuListener.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012-2014 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay, Naama Ben-David.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Naama Ben-David - Implementation.
+ */
 package edu.toronto.cs.se.modelepedia.graph_mavo.diagram.part;
 
 import java.util.ArrayList;
@@ -9,21 +20,28 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.ui.PlatformUI;
 
 import edu.toronto.cs.se.mavo.MAVOAlternative;
+import edu.toronto.cs.se.mavo.MAVOElement;
+import edu.toronto.cs.se.mavo.MAVOModel;
+import edu.toronto.cs.se.mavo.MayDecision;
 import edu.toronto.cs.se.mmint.MMINTException;
 import edu.toronto.cs.se.mmint.MMINTException.Type;
+import edu.toronto.cs.se.mmint.mavo.library.MAVOUtils;
+import edu.toronto.cs.se.mmint.mid.Model;
+import edu.toronto.cs.se.mmint.mid.library.MultiModelUtils;
 import edu.toronto.cs.se.mmint.mid.ui.GMFDiagramUtils;
-import edu.toronto.cs.se.mavo.MAVOElement;
-import edu.toronto.cs.se.modelepedia.graph_mavo.diagram.part.MAVODecisionTreeMenuListener.CreateNewObjectCommand;
-import edu.toronto.cs.se.modelepedia.graph_mavo.diagram.part.MAVODecisionTreeMenuListener.RemoveOldObjectCommand;
 
 public class MAVOAlternativeHighlightMenuListener extends SelectionAdapter {
 
@@ -65,6 +83,15 @@ public class MAVOAlternativeHighlightMenuListener extends SelectionAdapter {
 	}
 	
 	protected class HighlightAlternativeCommand extends AbstractTransactionalCommand{
+		
+		private static final int HIGHLIGHT_COLOR = 0x00FFFF;
+		private static final int FONT_HIGHLIGHT_COLOR = 0x000000;
+		private static final int GREYOUT_COLOR = 0xF4F4F4;
+		private static final int FONT_GREYOUT_COLOR = 0xD0D0D0;
+		private static final String EXAMPLE_SUFFIX = "_example";
+		private static final String DIAGRAM_ID = "edu.toronto.cs.se.modelepedia.graph_mavo.diagram.part.Graph_MAVODiagramEditorID";
+		private String diagramURI;
+		private String newDiagramURI;
 
 		public HighlightAlternativeCommand(TransactionalEditingDomain domain,
 				String label, List affectedFiles) {
@@ -74,10 +101,70 @@ public class MAVOAlternativeHighlightMenuListener extends SelectionAdapter {
 		@Override
 		protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
 				IAdaptable info) throws ExecutionException {
-			for (MAVOElement mavoElement: alternative.getMavoElements()){
-				
+			Diagram diag;
+			try{
+				diag = getDiagram(alternative);
+			}
+			catch (Exception e){
+				return CommandResult.newCancelledCommandResult();
+			}
+			List<View> diagramElements = new ArrayList<View>();
+			diagramElements.addAll(diag.getChildren());
+			diagramElements.addAll(diag.getEdges());
+			for (View diagElement: diagramElements){
+				MAVOElement modelElement = (MAVOElement) diagElement.getElement();
+				for (MAVOAlternative alt: modelElement.getAlternatives()){
+					if (alt.getFormulaVariable().equals(alternative.getFormulaVariable())){
+						MAVOUtils.colorElement(diagElement, HIGHLIGHT_COLOR, FONT_HIGHLIGHT_COLOR);
+						break;
+					}
+					else {
+						if (((MayDecision) alt.eContainer()).getFormulaVariable().equals(((MayDecision) alternative.eContainer()).getFormulaVariable())){
+							MAVOUtils.colorElement(diagElement, GREYOUT_COLOR, FONT_GREYOUT_COLOR);
+						}
+					}
+				}
+			}
+			
+			//Write diagram to file
+			try {
+				MultiModelUtils.createModelFile(diag, newDiagramURI, true);
+				GMFDiagramUtils.openGMFDiagram(newDiagramURI, DIAGRAM_ID, true);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			return CommandResult.newOKCommandResult();
+		}
+		
+		private Diagram getDiagram(MAVOAlternative alternative) throws MMINTException{
+			EObject model = alternative;
+			while (!(model instanceof MAVOModel)){
+				model = model.eContainer();
+			}
+			MAVOModel graphModel = (MAVOModel) model;
+			URI platformURI = graphModel.eResource().getURI();
+			String modelURI = platformURI.toPlatformString(false);
+			diagramURI = MultiModelUtils.replaceFileExtensionInUri(
+					modelURI,
+					"graphdiag_mavo"
+				);
+			
+			newDiagramURI = MultiModelUtils.addFileNameSuffixInUri(diagramURI, EXAMPLE_SUFFIX);
+
+			try {
+				MultiModelUtils.copyTextFileAndReplaceText(diagramURI, newDiagramURI, "", "", true);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			
+			Diagram d;
+			try{
+				d = (Diagram) MultiModelUtils.getModelFile(newDiagramURI, true);
+			} catch (Exception e) {
+				throw new MMINTException("There is no diagram!", e);
+			}
+			return d;
 		}
 		
 	}
