ca2606241521cb7429310f01a42c9a9af11f04f6
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-10-01 12:25:08 -0400

    tycho: Use github file as emftocsp repository

--- releng/edu.toronto.cs.se.mmint.releng/scripts/emftocsp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/atlanmod/EMFtoCSP/master/updateSite/fr.inria.atlanmod.emftocsp.03-09-13.zip
-mv fr.inria.atlanmod.emftocsp.03-09-13.zip /tmp/fr.inria.atlanmod.emftocsp.03-09-13.zip
