fc1d9f8e0118c668dca2695420f414c56468ef22
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-11-15 23:57:18 -0500

    mu-mmint: Show descriptions in decision outline view

--- plugins/Core/edu.toronto.cs.se.mmint.mavo/src/edu/toronto/cs/se/mmint/mavo/MAVODecisionLabelProvider.java
+++ plugins/Core/edu.toronto.cs.se.mmint.mavo/src/edu/toronto/cs/se/mmint/mavo/MAVODecisionLabelProvider.java
@@ -8,36 +8,47 @@
  * 
  * Contributors:
  *    Naama Ben-David - Implementation.
+ *    Alessio Di Sandro - Implementation.
  */
 package edu.toronto.cs.se.mmint.mavo;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 
-import edu.toronto.cs.se.mavo.LogicElement;
+import edu.toronto.cs.se.mavo.DecisionElement;
+import edu.toronto.cs.se.mavo.MAVOElement;
 import edu.toronto.cs.se.mavo.MAVOModel;
 
 public class MAVODecisionLabelProvider extends AdapterFactoryLabelProvider {
 
 	public MAVODecisionLabelProvider(AdapterFactory adapterFactory) {
+
 		super(adapterFactory);
 	}
-	
+
 	@Override
-	public String getText(Object object){
+	public String getText(Object object) {
+
 		String label;
-		if (object instanceof MAVOModel){
-			MAVOModel model = (MAVOModel) object;
-			label = model.eResource().getURI().toString();
+		if (object instanceof MAVOModel) {
+			MAVOModel rootModelObj = (MAVOModel) object;
+			label = rootModelObj.eResource().getURI().toString();
 		}
-		else if (object instanceof LogicElement){
-			LogicElement element = (LogicElement) object;
-			label = element.getFormulaVariable();
+		else if (object instanceof DecisionElement) {
+			DecisionElement decisionObj = (DecisionElement) object;
+			label = decisionObj.getFormulaVariable();
+			if (decisionObj.getDescription() != null) {
+				label += " (" + decisionObj.getDescription() + ")";
+			}
+		}
+		else if (object instanceof MAVOElement) {
+			MAVOElement modelObj = (MAVOElement) object;
+			label = modelObj.getFormulaVariable();
 		}
 		else {
 			label = super.getText(object);
 		}
-		
+
 		return label;
 	}
 
