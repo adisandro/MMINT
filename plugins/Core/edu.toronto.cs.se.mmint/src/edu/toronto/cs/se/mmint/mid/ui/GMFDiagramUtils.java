37547a6153e523b9cfb411780ffa9c25142fe4b7
Alessio Di Sandro <alessio.disandro@gmail.com>
2014-11-16 15:23:39 -0500

    mu-mmint: Fix some GMFDiagramUtils shortcomings
     * Proper detection of model file from diagram file
     * Move here functions for diagram highlighting and positioning
     * Better gmf diagram creation

--- plugins/Core/edu.toronto.cs.se.mmint/src/edu/toronto/cs/se/mmint/mid/ui/GMFDiagramUtils.java
+++ plugins/Core/edu.toronto.cs.se.mmint/src/edu/toronto/cs/se/mmint/mid/ui/GMFDiagramUtils.java
@@ -17,8 +17,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.ui.URIEditorInput;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -29,6 +31,7 @@ import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
 import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
 import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
 import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
+import org.eclipse.gmf.runtime.notation.BasicCompartment;
 import org.eclipse.gmf.runtime.notation.Connector;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.gmf.runtime.notation.FontStyle;
@@ -37,10 +40,14 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ui.PlatformUI;
 
+import edu.toronto.cs.se.mavo.MAVOElement;
 import edu.toronto.cs.se.mmint.MMINTException;
+import edu.toronto.cs.se.mmint.MMINTException.Type;
 import edu.toronto.cs.se.mmint.mid.ExtendibleElement;
 import edu.toronto.cs.se.mmint.mid.ExtendibleElementEndpoint;
+import edu.toronto.cs.se.mmint.mid.MIDLevel;
 import edu.toronto.cs.se.mmint.mid.constraint.MultiModelConstraintChecker;
+import edu.toronto.cs.se.mmint.mid.library.MultiModelRegistry;
 import edu.toronto.cs.se.mmint.mid.library.MultiModelUtils;
 
 public class GMFDiagramUtils {
@@ -97,7 +104,7 @@ public class GMFDiagramUtils {
 		return name;
 	}
 
-	public static void createGMFDiagram(String modelUri, String diagramUri, String diagramKind, String diagramPluginId, boolean isWorkspaceRelative) throws Exception {
+	public static Diagram createGMFDiagram(String modelUri, String diagramUri, String diagramKind, String diagramPluginId, boolean isWorkspaceRelative) throws Exception {
 
 		ResourceSet domainResourceSet = new ResourceSetImpl();
 		Resource modelResource = domainResourceSet.getResource(MultiModelUtils.getEMFUri(modelUri, isWorkspaceRelative), true);
@@ -109,11 +116,14 @@ public class GMFDiagramUtils {
 			diagramKind,
 			new PreferencesHint(diagramPluginId)
 		);
+		diagram.setName(MultiModelUtils.getLastSegmentFromUri(diagramUri));
 		diagramResource.getContents().add(diagram);
 		Map<String, Object> saveOptions = new HashMap<String, Object>();
 		saveOptions.put(XMLResource.OPTION_ENCODING, "UTF-8");
 		saveOptions.put(Resource.OPTION_SAVE_ONLY_IF_CHANGED, Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
 		diagramResource.save(saveOptions);
+
+		return diagram;
 	}
 
 	public static void openGMFDiagram(String diagramUri, String diagramId, boolean isWorkspaceRelative) throws Exception {
@@ -133,6 +143,37 @@ public class GMFDiagramUtils {
 		return adapterFactory;
 	}
 
+	private static void getDiagramFormulaVariable(@NonNull View diagramView, @NonNull Map<String, View> diagramViews) {
+
+		EObject modelObj = diagramView.getElement();
+		if (modelObj instanceof MAVOElement) {
+			diagramViews.put(((MAVOElement) modelObj).getFormulaVariable(), diagramView);
+		}
+	}
+
+	private static void getDiagramFormulaVariables(@NonNull EList<View> diagramViewList, @NonNull Map<String, View> diagramViews) {
+
+		for (View diagramView : diagramViewList) {
+			for (Object compartment : diagramView.getChildren()) {
+				if (compartment instanceof BasicCompartment) {
+					for (Object diagramSubview : ((BasicCompartment) compartment).getChildren()) {
+						getDiagramFormulaVariable((View) diagramSubview, diagramViews);
+					}
+				}
+			}
+			getDiagramFormulaVariable(diagramView, diagramViews);
+		}
+	}
+
+	public static @NonNull Map<String, View> getDiagramFormulaVariables(@NonNull Diagram diagram) {
+
+		Map<String, View> diagramViews = new HashMap<String, View>();
+		getDiagramFormulaVariables((EList<View>) diagram.getChildren(), diagramViews);
+		getDiagramFormulaVariables((EList<View>) diagram.getEdges(), diagramViews);
+
+		return diagramViews;
+	}
+
 	public static void colorDiagramElement(View diagramView, int color, int fontColor) throws MMINTException {
 
 		if (!(diagramView instanceof Shape) && !(diagramView instanceof Connector)) {
@@ -147,19 +188,50 @@ public class GMFDiagramUtils {
 				node.setFontColor(fontColor);
 			}
 		}
-		else if (diagramView instanceof Connector){
-			Connector line = (Connector) diagramView;
-			line.setLineColor(color);
-			FontStyle labelFont = (FontStyle) line.getStyles().get(0);
+		else if (diagramView instanceof Connector) {
+			Connector edge = (Connector) diagramView;
+			edge.setLineColor(color);
+			FontStyle labelFont = (FontStyle) edge.getStyles().get(0);
 			if (fontColor >= 0) {
 				labelFont.setFontColor(fontColor);
 			}
 		}
 	}
 
-	public static @NonNull IFile getModelIFile(@NonNull IFile diagramFile) {
+	public static void copyDiagramElementPosition(@NonNull View oldDiagramView, @NonNull View newDiagramView) throws MMINTException {
 
-		IFile modelFile = diagramFile.getParent().getFile(new Path(diagramFile.getName().substring(0, diagramFile.getName().length() - DIAGRAM_SUFFIX.length())));
+		boolean
+			oldShape = oldDiagramView instanceof Shape,
+			newShape = newDiagramView instanceof Shape,
+			oldConnector = oldDiagramView instanceof Connector,
+			newConnector = newDiagramView instanceof Connector;
+		if (!oldShape && !oldConnector) {
+			throw new MMINTException("Old diagram element not supported: " + newDiagramView.getClass().getName());
+		}
+		if (!newShape && !newConnector) {
+			throw new MMINTException("New diagram element not supported: " + newDiagramView.getClass().getName());
+		}
+		if (!((oldShape && newShape) || (oldConnector && newConnector))) {
+			throw new MMINTException("Old and new diagram elements don't match");
+		}
+
+		if (newDiagramView instanceof Shape) {
+			Shape oldNode = (Shape) oldDiagramView, newNode = (Shape) newDiagramView;
+			newNode.setLayoutConstraint(oldNode.getLayoutConstraint());
+		}
+		else if (newDiagramView instanceof Connector) {
+			Connector oldEdge = (Connector) oldDiagramView, newEdge = (Connector) newDiagramView;
+			newEdge.setBendpoints(oldEdge.getBendpoints());
+			newEdge.setSourceAnchor(oldEdge.getSourceAnchor());
+			newEdge.setTargetAnchor(oldEdge.getTargetAnchor());
+		}
+	}
+
+	public static @NonNull IFile getModelIFile(@NonNull IFile diagramFile) throws Exception {
+
+		Diagram diagram = (Diagram) MultiModelUtils.getModelFile(diagramFile.getFullPath().toString(), true);
+		String modelUri = MultiModelRegistry.getModelAndModelElementUris(diagram.getElement(), MIDLevel.INSTANCES)[0];
+		IFile modelFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(modelUri));
 
 		return modelFile;
 	}
@@ -170,7 +242,12 @@ public class GMFDiagramUtils {
 		IFile diagramFile = (IFile) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor().getEditorInput().getAdapter(IFile.class);
 		if (diagramFile != null) {
 			files.add(diagramFile);
-			files.add(getModelIFile(diagramFile));
+			try {
+				files.add(getModelIFile(diagramFile));
+			}
+			catch (Exception e) {
+				MMINTException.print(Type.WARNING, "Can't add model file of diagram " + diagramFile.getName() + " for gmf transactional command", e);
+			}
 		}
 
 		return files;
