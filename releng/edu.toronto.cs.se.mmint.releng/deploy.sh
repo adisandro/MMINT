fa3400300bbc84659f8f7e48127d2f0d06692e83
Alessio Di Sandro <alessio.disandro@gmail.com>
2015-01-05 14:27:10 -0500

    releng: Revert previous commit
     * Create ssh key pair for user hudson instead

--- releng/edu.toronto.cs.se.mmint.releng/deploy.sh
+++ releng/edu.toronto.cs.se.mmint.releng/deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-ssh -i /home/alessio/.ssh/id_rsa modelling@colony.cs.toronto.edu 'rm -rf ~/site/www/mmint/updates/'
+ssh modelling@colony.cs.toronto.edu 'rm -rf ~/site/www/mmint/updates/'
 scp -r releng/edu.toronto.cs.se.mmint.install/target/repository/ modelling@colony.cs.toronto.edu:~/site/www/mmint/updates/
