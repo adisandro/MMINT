365b55e0506c07ecfad09d3a89e1b04e2cac4093
Alessio Di Sandro <alessio.disandro@gmail.com>
2015-01-05 12:07:07 -0500

    releng: Add shell header to deploy script

--- releng/edu.toronto.cs.se.mmint.releng/deploy.sh
+++ releng/edu.toronto.cs.se.mmint.releng/deploy.sh
@@ -1,2 +1,3 @@
+#! /bin/sh
 ssh modelling@colony.cs.toronto.edu 'rm -rf ~/site/www/mmint/updates/'
 scp -r releng/edu.toronto.cs.se.mmint.install/target/repository/ modelling@colony.cs.toronto.edu:~/site/www/mmint/updates/
