ede3dd7af1b547b561643284218d5457103f3833
Alessio Di Sandro <alessio.disandro@gmail.com>
2015-01-13 11:22:21 -0500

    misc: Update copyright notice to 2015

--- plugins/Types/StateMachine/edu.toronto.cs.se.modelepedia.statemachine.diagram/src-gen/edu/toronto/cs/se/modelepedia/statemachine/diagram/part/DiagramEditorContextMenuProvider.java
+++ plugins/Types/StateMachine/edu.toronto.cs.se.modelepedia.statemachine.diagram/src-gen/edu/toronto/cs/se/modelepedia/statemachine/diagram/part/DiagramEditorContextMenuProvider.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Text ->
  * 
- * Copyright (c) 2012-2014 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Copyright (c) 2012-2015 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
  * Rick Salay.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
