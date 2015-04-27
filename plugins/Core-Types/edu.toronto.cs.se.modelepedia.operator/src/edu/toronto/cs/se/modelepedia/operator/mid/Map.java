d6170039d372ddfde22a0b7c7f4a915dde94401a
Alessio Di Sandro <alessio.disandro@gmail.com>
2015-04-26 23:54:50 -0400

    megamodels: Implement mid operators (6)
     * Add proper cartesian product of input models in map

--- plugins/Core-Types/edu.toronto.cs.se.modelepedia.operator/src/edu/toronto/cs/se/modelepedia/operator/mid/Map.java
+++ plugins/Core-Types/edu.toronto.cs.se.modelepedia.operator/src/edu/toronto/cs/se/modelepedia/operator/mid/Map.java
@@ -13,7 +13,9 @@ package edu.toronto.cs.se.modelepedia.operator.mid;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.common.util.BasicEList;
@@ -35,52 +37,80 @@ import edu.toronto.cs.se.mmint.mid.operator.impl.OperatorImpl;
 
 public class Map extends OperatorImpl {
 
+	private class InputModel {
+		private Model model;
+		private List<ConversionOperator> conversions;
+		public InputModel(@NonNull Model model, @NonNull List<ConversionOperator> conversions) {
+			this.model = model;
+			this.conversions = conversions;
+		}
+		public @NonNull Model getModel() {
+			return model;
+		}
+		public @NonNull List<ConversionOperator> getConversions() {
+			return conversions;
+		}
+	}
+
 	@NonNull private final static String MAP_MID_SUFFIX = "_map";
 
-	private List<List<Model>> x(Operator operatorType, MultiModel mid, List<List<List<ConversionOperator>>> conversions) {
+	private List<Set<InputModel>> x(Operator operatorType, MultiModel mid) {
 
 		//TODO MMINT[MAP] Add support for upper bound = -1
 		//TODO MMINT[MAP] Need something more in the operators themselves to validate proper input in case of model rels
-		List<List<Model>> y = new ArrayList<>();
+		List<Set<InputModel>> y = new ArrayList<>();
 		for (ModelEndpoint inputModelTypeEndpoint : operatorType.getInputs()) {
 			Model inputModelType = inputModelTypeEndpoint.getTarget();
-			List<Model> yy = new ArrayList<>();
+			Set<InputModel> yy = new HashSet<>();
 			y.add(yy);
-			List<List<ConversionOperator>> zz = new ArrayList<>();
-			conversions.add(zz);
 			for (Model inputModel : mid.getModels()) {
 				List<ConversionOperator> conversionOperatorTypes = MultiModelTypeHierarchy.instanceOf(inputModel, inputModelType.getUri());
 				if (conversionOperatorTypes == null) {
 					continue;
 				}
-				zz.add(conversionOperatorTypes);
-				yy.add(inputModel);
+				yy.add(new InputModel(inputModel, conversionOperatorTypes));
 			}
 		}
 
 		return y;
 	}
 
-	private void y(Operator operatorType, MultiModel mid, List<List<Model>> midInputs, List<List<List<ConversionOperator>>> midConversions) {
+	private void in(List<Set<InputModel>> midInputs, List<InputModel> accumulator, int index, Set<List<InputModel>> result) {
 
-		EList<Model> inputModels = new BasicEList<>();
-		java.util.Map<Integer, EList<ConversionOperator>> conversions = new HashMap<>();
-		for (int i = 0; i < midInputs.size(); i++) {
-			int cartesianIndex = 0;//TODO MMINT[MAP] Select by cartesian product
-			List<Model> a = midInputs.get(i);
-			inputModels.add(a.get(cartesianIndex));
-			List<List<ConversionOperator>> b = midConversions.get(i);
-			List<ConversionOperator> bb = b.get(cartesianIndex);
-			if (!bb.isEmpty()) {
-				conversions.put(new Integer(i), new BasicEList<>(bb));
+		if (midInputs.size() == index) {
+			result.add(accumulator);
+		}
+		else {
+			for (InputModel input : midInputs.get(index)) {
+				List<InputModel> newAccumulator = new ArrayList<>(accumulator);
+				newAccumulator.add(input);
+				in(midInputs, newAccumulator, index + 1, result);
 			}
 		}
-		try {
-			//TODO MMINT[OPERATOR] An operator instance should be created here
-			operatorType.run(inputModels, conversions, mid);
-		}
-		catch (Exception e) {
-			MMINTException.print(IStatus.WARNING, "Operator " + operatorType + " execution error, skipping it", e);
+	}
+
+	private void out(Operator operatorType, MultiModel mid, List<Set<InputModel>> midInputs) {
+
+		Set<List<InputModel>> result = new HashSet<>();
+		in(midInputs, new ArrayList<>(), 0, result);
+		for (List<InputModel> res : result) {
+			EList<Model> inputModels = new BasicEList<>();
+			java.util.Map<Integer, EList<ConversionOperator>> conversions = new HashMap<>();
+			for (int i = 0; i < res.size(); i++) {
+				InputModel im = res.get(i);
+				inputModels.add(im.getModel());
+				List<ConversionOperator> c = im.getConversions();
+				if (!c.isEmpty()) {
+					conversions.put(new Integer(i), new BasicEList<>(c));
+				}
+			}
+			try {
+				//TODO MMINT[OPERATOR] An operator instance should be created here
+				operatorType.run(inputModels, conversions, mid);
+			}
+			catch (Exception e) {
+				MMINTException.print(IStatus.WARNING, "Operator " + operatorType + " execution error, skipping it", e);
+			}
 		}
 	}
 
@@ -92,20 +122,11 @@ public class Map extends OperatorImpl {
 		MultiModel mapMid = (MultiModel) midModel.getEMFInstanceRoot();
 
 		// find all possible combinations of inputs for operatorType and execute them
-		List<List<List<ConversionOperator>>> conversions = new ArrayList<>();
-		List<List<Model>> midInputs = x(operatorType, mapMid, conversions);
-		y(operatorType, mapMid, midInputs, conversions);
-
-		// 
+		List<Set<InputModel>> midInputs = x(operatorType, mapMid);
+		out(operatorType, mapMid, midInputs);
 		/* TODO
-		 * 2) Apply operatorType to the mid
-		 * 2a) find all possible combinations of the input of operatorType
-		 *     (== matches, list of lists of actual parameters List<EList<Model>>)
-		 *     use some heuristics to have groups of actual parameters, then invoke getExecutables() to check
-		 * 2b) execute the operatorType on the matches and store the output mid
-		 * 3) Make it recursive and add option for shallow/deep
-		 * 4) Traceability: create Map<OperatorType>, but what about the individual applications of OperatorType?
-		 * 5) Think about the alternative invocation, with a mid for every parameter of operatorType (no need to find matches?)
+		 * - Make it recursive and add option for shallow/deep
+		 * - Think about the alternative invocation, with a mid for every parameter of operatorType (no need to find matches?)
 		 */
 
 		String mapMidUri = MultiModelUtils.getUniqueUri(
