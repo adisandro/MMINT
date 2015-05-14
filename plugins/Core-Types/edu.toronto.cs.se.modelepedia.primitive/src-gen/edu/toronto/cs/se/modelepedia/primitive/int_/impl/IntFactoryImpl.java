80e290f768814d307661cbddfaa3f42f95fe0c99
Alessio Di Sandro <alessio.disandro@gmail.com>
2015-05-14 12:05:59 -0400

    megamodels: Implement mid operators (38)
     * Add projects for primitive types and Int model type

---
+++ plugins/Core-Types/edu.toronto.cs.se.modelepedia.primitive/src-gen/edu/toronto/cs/se/modelepedia/primitive/int_/impl/IntFactoryImpl.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2012-2015 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
+package edu.toronto.cs.se.modelepedia.primitive.int_.impl;
+
+import edu.toronto.cs.se.modelepedia.primitive.int_.*;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class IntFactoryImpl extends EFactoryImpl implements IntFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static IntFactory init() {
+		try {
+			IntFactory theIntFactory = (IntFactory)EPackage.Registry.INSTANCE.getEFactory(IntPackage.eNS_URI);
+			if (theIntFactory != null) {
+				return theIntFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new IntFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public IntFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case IntPackage.INT: return createInt();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Int createInt() {
+		IntImpl int_ = new IntImpl();
+		return int_;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public IntPackage getIntPackage() {
+		return (IntPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static IntPackage getPackage() {
+		return IntPackage.eINSTANCE;
+	}
+
+} //IntFactoryImpl
