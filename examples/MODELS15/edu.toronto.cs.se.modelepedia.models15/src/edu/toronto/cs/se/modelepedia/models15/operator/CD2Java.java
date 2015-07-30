4f4ce1a93b9d887173d1f1387f8cd156ef3f5f23
Alessio Di Sandro <alessio.disandro@gmail.com>
2015-07-30 03:48:58 +0200

    models15: Add CD2Java conversion

---
+++ examples/MODELS15/edu.toronto.cs.se.modelepedia.models15/src/edu/toronto/cs/se/modelepedia/models15/operator/CD2Java.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2012-2015 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
+package edu.toronto.cs.se.modelepedia.models15.operator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.jdt.annotation.NonNull;
+
+import edu.toronto.cs.se.mmint.MultiModelTypeRegistry;
+import edu.toronto.cs.se.mmint.mid.GenericElement;
+import edu.toronto.cs.se.mmint.mid.Model;
+import edu.toronto.cs.se.mmint.mid.MultiModel;
+import edu.toronto.cs.se.mmint.mid.library.MultiModelOperatorUtils;
+import edu.toronto.cs.se.mmint.mid.library.MultiModelUtils;
+import edu.toronto.cs.se.mmint.mid.operator.Operator;
+import edu.toronto.cs.se.mmint.mid.operator.OperatorInput;
+import edu.toronto.cs.se.mmint.mid.operator.impl.ConversionOperatorImpl;
+import edu.toronto.cs.se.mmint.mid.relationship.ModelRel;
+import edu.toronto.cs.se.modelepedia.operator.patch.ModelRelTypeTransformation;
+
+public class CD2Java extends ConversionOperatorImpl {
+
+	// input-output
+	private final static @NonNull String IN_MODEL = "cd";
+	private final static @NonNull String OUT_MODEL = "java";
+	// constants
+	private final static @NonNull String MODELRELTYPETRANSFORMATION_OPERATOR_URI = "http://se.cs.toronto.edu/modelepedia/Operator_ModelRelTypeTransformation";
+	private final static @NonNull String CD2JAVA_MODELRELTYPE_URI = "http://se.cs.toronto.edu/modelepedia/CD2Java";
+
+	// output
+	private Model javaModel;
+
+	private @NonNull Model x(Model cdModel, MultiModel instanceMID) throws Exception {
+
+		Operator transformationOperatorType = MultiModelTypeRegistry.getType(MODELRELTYPETRANSFORMATION_OPERATOR_URI);
+		EList<Model> inputModels = new BasicEList<>();
+		inputModels.add(cdModel);
+		EList<OperatorInput> inputs = transformationOperatorType.checkAllowedInputs(inputModels);
+		Map<String, MultiModel> outputMIDsByName = MultiModelOperatorUtils.createSimpleOutputMIDsByName(transformationOperatorType, instanceMID);
+		Operator transformationOperator = transformationOperatorType.start(inputs, outputMIDsByName, null);
+		ModelRel cd2javaRelType = MultiModelTypeRegistry.getType(CD2JAVA_MODELRELTYPE_URI);
+		transformationOperatorType.isAllowedTargetGeneric(transformationOperatorType.getGenerics().get(0), cd2javaRelType, inputs);
+
+		return transformationOperator.getOutputsByName().get(ModelRelTypeTransformation.OUT_MODEL);
+	}
+
+	@Override
+	public Map<String, Model> run(
+			Map<String, Model> inputsByName, Map<String, GenericElement> genericsByName,
+			Map<String, MultiModel> outputMIDsByName) throws Exception {
+
+		// input
+		Model cdModel = (ModelRel) inputsByName.get(IN_MODEL);
+		MultiModel instanceMID = outputMIDsByName.get(OUT_MODEL);
+
+		// run model rel type transformation
+		javaModel = x(cdModel, instanceMID);
+
+		// output
+		Map<String, Model> outputsByName = new HashMap<>();
+		outputsByName.put(OUT_MODEL, javaModel);
+
+		return outputsByName;
+	}
+
+	@Override
+	public void cleanup() throws Exception {
+
+		if (javaModel != null) {
+			MultiModelUtils.deleteModelFile(javaModel);
+			javaModel = null;
+		}
+	}
+
+}
