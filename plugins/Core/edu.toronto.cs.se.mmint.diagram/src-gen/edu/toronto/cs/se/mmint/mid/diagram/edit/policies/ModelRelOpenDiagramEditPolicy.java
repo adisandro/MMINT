d39d4748978a2a3573e9bec9805e476bbc020c88
Alessio Di Sandro <alessio.disandro@gmail.com>
2016-04-20 21:06:36 -0400

    releng: Prepare for future release
     * Update copyright to 2016
     * Bump version to 2.0.0
     * Update tycho to 0.25

--- plugins/Core/edu.toronto.cs.se.mmint.diagram/src-gen/edu/toronto/cs/se/mmint/mid/diagram/edit/policies/ModelRelOpenDiagramEditPolicy.java
+++ plugins/Core/edu.toronto.cs.se.mmint.diagram/src-gen/edu/toronto/cs/se/mmint/mid/diagram/edit/policies/ModelRelOpenDiagramEditPolicy.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2012-2015 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Copyright (c) 2012-2016 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
  * Rick Salay.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
