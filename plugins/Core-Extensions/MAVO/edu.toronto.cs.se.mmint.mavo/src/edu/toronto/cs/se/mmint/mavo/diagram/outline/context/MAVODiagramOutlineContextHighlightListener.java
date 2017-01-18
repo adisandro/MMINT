2770e5777e244ff32210167fbc051856d5f9f591
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-01-17 20:50:00 -0500

    releng: Update copyright to 2017

--- plugins/Core-Extensions/MAVO/edu.toronto.cs.se.mmint.mavo/src/edu/toronto/cs/se/mmint/mavo/diagram/outline/context/MAVODiagramOutlineContextHighlightListener.java
+++ plugins/Core-Extensions/MAVO/edu.toronto.cs.se.mmint.mavo/src/edu/toronto/cs/se/mmint/mavo/diagram/outline/context/MAVODiagramOutlineContextHighlightListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012-2016 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Copyright (c) 2012-2017 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
  * Rick Salay, Naama Ben-David.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
