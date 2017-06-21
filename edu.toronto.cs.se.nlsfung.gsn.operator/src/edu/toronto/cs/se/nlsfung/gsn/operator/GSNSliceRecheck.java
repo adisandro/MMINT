f69d2b9a18b07831a4c7414067ccec2bd05d3297
Nick Fung <nlsfung@cs.toronto.edu>
2017-06-21 14:16:12 +0800

    Re-implemented GSNSliceRevise and GSNSliceRecheck.

--- edu.toronto.cs.se.nlsfung.gsn.operator/src/edu/toronto/cs/se/nlsfung/gsn/operator/GSNSliceRecheck.java
+++ edu.toronto.cs.se.nlsfung.gsn.operator/src/edu/toronto/cs/se/nlsfung/gsn/operator/GSNSliceRecheck.java
@@ -12,56 +12,136 @@
  */
 package edu.toronto.cs.se.nlsfung.gsn.operator;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
 
 import edu.toronto.cs.se.modelepedia.operator.slice.Slice;
-import edu.toronto.cs.se.nlsfung.gsn.Context;
-import edu.toronto.cs.se.nlsfung.gsn.Goal;
-import edu.toronto.cs.se.nlsfung.gsn.Solution;
-import edu.toronto.cs.se.nlsfung.gsn.Strategy;
+import edu.toronto.cs.se.nlsfung.gsn.ContextualElement;
+import edu.toronto.cs.se.nlsfung.gsn.CoreElement;
+import edu.toronto.cs.se.nlsfung.gsn.DecomposableCoreElement;
+import edu.toronto.cs.se.nlsfung.gsn.InContextOf;
+import edu.toronto.cs.se.nlsfung.gsn.SafetyCase;
+import edu.toronto.cs.se.nlsfung.gsn.SupportedBy;
 
 public class GSNSliceRecheck extends Slice {
 
-	// Adds impacted model elements reachable from a single model element
 	@Override
-	public void addImpactedModelElems(EObject elem, Set<EObject> impacted) {
-
-		if (elem instanceof Goal) {
-			Goal g = (Goal) elem;
-			
-			// Impacted: conclusion goals linked via the strategy it supports
-			Strategy s = (Strategy) g.getSupports();
-			if (s != null) {
-				Goal gSuper = s.getSupports();
-				if (!impacted.contains(gSuper)) {
-					impacted.add(gSuper);
-					addImpactedModelElems(gSuper, impacted);				
-				}				
-			}
-		}
+	// Get all model elements that are potentially impacted by the input set
+	// (and therefore needs to be rechecked).
+	public Set<EObject> getAllImpactedElements(Set<EObject> elemSet) {
+		Set<EObject> impactedAll = new HashSet<>();
+		Set<EObject> impactedCur = new HashSet<>();
+		Set<EObject> impactedNext = new HashSet<>();
 		
-		else if (elem instanceof Strategy) {
-		    // no impacted elements
-		}
-		else if (elem instanceof Solution) {
-			Solution s = (Solution) elem;
-			
-            // Impacted: Goals supported by the solution.
-			for (Goal g : s.getSupports()) {
-				if (!impacted.contains(g)) {
-					impacted.add(g);
-					addImpactedModelElems(g, impacted);
+		// Iterate through the current set of newly added model elements 
+		// to identify all others that may be potentially impacted. 
+		impactedAll.addAll(elemSet);
+		impactedCur.addAll(elemSet);
+		while (!impactedCur.isEmpty()) {
+			for (EObject elem : impactedCur) {
+				// Get all model elements directly impacted by the current 
+				// one in consideration and remove duplicates.
+				for (EObject newElem : getDirectlyImpactedElements(elem)) {
+					if (!impactedAll.contains(newElem)) {
+						impactedAll.add(newElem);
+						impactedNext.add(newElem);
+					}
 				}
 			}
-		}
-		else if (elem instanceof Context) {
-            // no impacted elements
+			
+			// Prepare for next iteration.
+			impactedCur.clear();
+			impactedCur.addAll(impactedNext);
+			impactedNext.clear();
 		}
 		
-		// Remove any null pointers from the impacted set.
+		return impactedAll;
+	}
+	
+	// Get impacted model elements directly reachable from the input element.
+	@Override
+	public Set<EObject> getDirectlyImpactedElements(EObject elem) {
+		Set<EObject> impacted = new HashSet<>();
+
+		// The input element itself is always impacted.
+		impacted.add(elem);
+
+		// If input is a safety case, then the following are also impacted:
+		// 1) Owned goals, strategies, solutions, contexts and ASILs.
+		if (elem instanceof SafetyCase) {
+			SafetyCase sc = (SafetyCase) elem;
+			impacted.addAll(sc.getGoals());
+			impacted.addAll(sc.getStrategies());
+			impacted.addAll(sc.getSolutions());
+			impacted.addAll(sc.getContexts());
+			impacted.addAll(sc.getASILLevels());
+			
+		// If input is a core element (i.e. goal, strategy or solution), then the 
+		// SupportedBy relations that it supports are also impacted.
+		} else if (elem instanceof CoreElement) {
+			CoreElement c = (CoreElement) elem;
+			impacted.addAll(c.getSupports());
+			
+		// If input is a contextual element, then its InContextOf relations
+		// are also impacted.
+		} else if (elem instanceof ContextualElement) {
+			ContextualElement c = (ContextualElement) elem;
+			impacted.addAll(c.getContextOf());
+		
+		// If input is a SupportedBy relation, then its conclusion is impacted.	
+		} else if (elem instanceof SupportedBy) {
+			SupportedBy rel = (SupportedBy) elem;
+			impacted.add(rel.getConclusion());
+			
+		// If input is a InContextOf relation, then the decomposable core 
+		// elements with or inherit this context are also impacted.
+		} else if (elem instanceof InContextOf) {
+			InContextOf rel = (InContextOf) elem;
+			impacted.addAll(getDescendants(rel.getContextOf()));
+		}
+
+		// Remove possible null element from impacted set.
 		impacted.remove(null);
+		
+		return impacted;
+	}	
+	
+	// Adds all the descendants of the input decomposable core element (including 
+	// itself and connecting relations) to the input result set.
+	public Set<EObject> getDescendants(DecomposableCoreElement elem) {
+		Set<EObject> descendantsAll = new HashSet<>();
+		Set<EObject> descendantsCur = new HashSet<>();
+		Set<EObject> descendantsNext = new HashSet<>();
+		
+		// Iterate through the current set of newly added descendants
+		// to identify the next generation of descendants.
+		descendantsAll.add(elem);
+		descendantsCur.add(elem);
+		while (!descendantsCur.isEmpty()) {
+			for (EObject curElem : descendantsCur) {
+				if (curElem instanceof DecomposableCoreElement) {
+					DecomposableCoreElement d = (DecomposableCoreElement) curElem;
+					descendantsNext.add(d.getSupportedBy());
+				} else if (curElem instanceof SupportedBy) {
+					SupportedBy s = (SupportedBy) curElem;
+					descendantsNext.addAll(s.getPremises());
+				}
+			}
+			
+			descendantsCur.clear();
+			for (EObject newElem : descendantsNext) {
+				if (!descendantsAll.contains(newElem)) {
+					descendantsAll.add(newElem);
+					descendantsCur.add(newElem);
+				}
+			}
+			
+			descendantsNext.clear();
+		}
+		
+		return descendantsAll;
 	}
 
 }
--- edu.toronto.cs.se.nlsfung.gsn.operator/src/edu/toronto/cs/se/nlsfung/gsn/operator/GSNSliceRevise.java
+++ edu.toronto.cs.se.nlsfung.gsn.operator/src/edu/toronto/cs/se/nlsfung/gsn/operator/GSNSliceRevise.java
@@ -18,85 +18,173 @@ import java.util.Set;
 import org.eclipse.emf.ecore.EObject;
 
 import edu.toronto.cs.se.modelepedia.operator.slice.Slice;
-import edu.toronto.cs.se.nlsfung.gsn.Context;
 import edu.toronto.cs.se.nlsfung.gsn.ContextualElement;
-import edu.toronto.cs.se.nlsfung.gsn.CoreElement;
+import edu.toronto.cs.se.nlsfung.gsn.DecomposableCoreElement;
 import edu.toronto.cs.se.nlsfung.gsn.Goal;
-import edu.toronto.cs.se.nlsfung.gsn.GoalSupporter;
+import edu.toronto.cs.se.nlsfung.gsn.InContextOf;
+import edu.toronto.cs.se.nlsfung.gsn.SafetyCase;
 import edu.toronto.cs.se.nlsfung.gsn.Solution;
 import edu.toronto.cs.se.nlsfung.gsn.Strategy;
-import edu.toronto.cs.se.nlsfung.gsn.StrategySupporter;
+import edu.toronto.cs.se.nlsfung.gsn.SupportedBy;
 
 public class GSNSliceRevise extends Slice {
-
-	// Adds impacted model elements reachable from a single model element
+	
 	@Override
-	public void addImpactedModelElems(EObject elem, Set<EObject> impacted) {
-
-		if (elem instanceof Goal) {
-			Goal g = (Goal) elem;
-			
-			// Impacted: 
-			// 1) All strategies the goal supports or is supported by. 
-			// 2) All solutions the goal is supported by.
-			impacted.add(g.getSupportedBy());
-			impacted.add(g.getSupports());
-			
-		} else if (elem instanceof Strategy) {
-		    Strategy s = (Strategy) elem;
-		    
-		    // Impacted: All goals the strategy supports or is supported by.
-			impacted.add(s.getSupports());
-			impacted.addAll(s.getSupportedBy());
-		    
-		} else if (elem instanceof Solution) {
-			Solution s = (Solution) elem;
-			
-            // Impacted: Goals supported by the solution.
-			impacted.addAll(s.getSupports());
-		}
-		else if (elem instanceof Context) {
-			Context c = (Context) elem;
-			
-            // Impacted:
-			// 1) All goals, strategies and solutions that are in this context.
-			// 2) All goals, strategies and solutions that inherit this context.			
-			Set<CoreElement> descendants = new HashSet<>();
-			for (CoreElement coreElem : c.getContextOf()) {
-				addDescendants(coreElem, descendants);
-			}
-			impacted.addAll(descendants);
-			impacted.addAll(c.getContextOf());
+	// Get all model elements in a safety case that needs to be re-checked 
+	// given the input set of elements that needs to be revised.
+	public Set<EObject> getAllImpactedElements(Set<EObject> elemSet) {
+		Set<EObject> impactedAll = new HashSet<>();
+		
+		// Iterate through the input set of revised elements to identify 
+		// all model elements that require re-checking.
+		for (EObject elem : elemSet) {
+			impactedAll.add(elem);
+			impactedAll.addAll(getDirectlyImpactedElements(elem));
 		}
 		
-		// Remove any null pointers from the impacted set.
-		impacted.remove(null);
+		return impactedAll;
 	}
 	
-	
-	// Adds all the descendants of the input core element to the input result set.
-	public void addDescendants(CoreElement elem, Set<CoreElement> result) {
-		if (elem instanceof Goal) {
-			Goal g = (Goal) elem;
-			CoreElement descendant = g.getSupportedBy();
-			if (!result.contains(descendant) && descendant != null) {
-				result.add(descendant);
-				addDescendants(descendant, result);
+	// Get impacted model elements directly reachable from the input element.
+	@Override
+	public Set<EObject> getDirectlyImpactedElements(EObject elem) {
+		Set<EObject> impacted = new HashSet<>();
+
+		// The input element itself is always impacted.
+		impacted.add(elem);
+		
+		// If input is a safety case, then the following are also impacted:
+		// 1) Everything in the safety case.
+		if (elem instanceof SafetyCase) {
+			SafetyCase sc = (SafetyCase) elem;
+			impacted.addAll(sc.getSolutions());
+			impacted.addAll(sc.getContexts());
+			impacted.addAll(sc.getASILLevels());
+			
+			Set<DecomposableCoreElement> decompElemSet = new HashSet<>();
+			decompElemSet.addAll(sc.getGoals());
+			decompElemSet.addAll(sc.getStrategies());
+			
+			impacted.addAll(decompElemSet);
+			
+			for (DecomposableCoreElement decompElem : decompElemSet) {
+				impacted.add(decompElem.getInContextOf());
+				impacted.add(decompElem.getSupportedBy());
 			}
 			
+		// If input is a goal, then the following are also impacted:
+		// 1) All SupportedBy relations connected to it.
+		// 2) All model elements connected to the sliced relations.
+		} else if (elem instanceof Goal) {
+			Goal g = (Goal) elem;
+			Set<SupportedBy> impactedRelSet = new HashSet<>();
+			impactedRelSet.add(g.getSupportedBy());
+			impactedRelSet.addAll(g.getSupports());
+			
+			impacted.addAll(impactedRelSet);
+			for (SupportedBy rel : impactedRelSet) {
+				impacted.add(rel.getConclusion());
+				impacted.addAll(rel.getPremises());
+			}
+		
+		// If input is a strategy, then the following are also impacted:
+		// 1) All SupportedBy relations connected to it.
+		// 2) All model elements connected to the sliced relations.
 		} else if (elem instanceof Strategy) {
 			Strategy s = (Strategy) elem;
-			if (s.getSupportedBy() != null) {
-				for (CoreElement descendant : s.getSupportedBy()) {
-					if (!result.contains(descendant) && descendant != null) {
-						result.add(descendant);
-						addDescendants(descendant, result);
-					}
+			Set<SupportedBy> impactedRelSet = new HashSet<>();
+			impactedRelSet.add(s.getSupportedBy());
+			impactedRelSet.addAll(s.getSupports());
+			
+			impacted.addAll(impactedRelSet);
+			for (SupportedBy rel : impactedRelSet) {
+				impacted.add(rel.getConclusion());
+				impacted.addAll(rel.getPremises());
+			}
+			
+		// If input is a contextual element (Context or ASIL), 
+		// then the following are also impacted:
+		// 1) All InContextOf relations connected to it.
+		// 2) All model elements that introduce C as the context.
+		// 3) All model elements that inherit C as the context 
+		//    (and connecting relations).
+		} else if (elem instanceof ContextualElement) {
+			ContextualElement c = (ContextualElement) elem;
+			Set<InContextOf> impactedRelSet = new HashSet<>();
+			impactedRelSet.addAll(c.getContextOf());
+			
+			impacted.addAll(impactedRelSet);
+			for (InContextOf rel : impactedRelSet) {
+				impacted.addAll(getDescendants(rel.getContextOf()));
+			}
+			
+		// If input is a solution, then the following are also impacted:
+		// 1) All SupportedBy relations connected to it.
+		// 2) All goals connected to the solution via the sliced relations.
+		} else if (elem instanceof Solution) {
+			Solution s = (Solution) elem;
+			Set<SupportedBy> impactedRelSet = new HashSet<>();
+			impactedRelSet.addAll(s.getSupports());
+			
+			impacted.addAll(impactedRelSet);
+			for (SupportedBy rel : impactedRelSet) {
+				impacted.add(rel.getConclusion());
+				impacted.addAll(rel.getPremises());
+			}
+			
+		// If input is a SupportedBy relation, then its source and 
+		// target model element is potentially impacted.
+		} else if (elem instanceof SupportedBy) {
+			SupportedBy rel = (SupportedBy) elem;
+			impacted.add(rel.getConclusion());
+			impacted.addAll(rel.getPremises());
+			
+		// If input is a InContextOf relation, then the decomposable core 
+		// elements with or inherit this context are also impacted.
+		} else if (elem instanceof InContextOf) {
+			InContextOf rel = (InContextOf) elem;
+			impacted.addAll(getDescendants(rel.getContextOf()));
+		}
+		
+		// Remove possible null element from impacted set.
+		impacted.remove(null);
+		
+		return impacted;
+	}
+
+	// Adds all the descendants of the input decomposable core element (including 
+	// itself and connecting relations) to the input result set.
+	public Set<EObject> getDescendants(DecomposableCoreElement elem) {
+		Set<EObject> descendantsAll = new HashSet<>();
+		Set<EObject> descendantsCur = new HashSet<>();
+		Set<EObject> descendantsNext = new HashSet<>();
+		
+		// Iterate through the current set of newly added descendants
+		// to identify the next generation of descendants.
+		descendantsAll.add(elem);
+		descendantsCur.add(elem);
+		while (!descendantsCur.isEmpty()) {
+			for (EObject curElem : descendantsCur) {
+				if (curElem instanceof DecomposableCoreElement) {
+					DecomposableCoreElement d = (DecomposableCoreElement) curElem;
+					descendantsNext.add(d.getSupportedBy());
+				} else if (curElem instanceof SupportedBy) {
+					SupportedBy s = (SupportedBy) curElem;
+					descendantsNext.addAll(s.getPremises());
 				}
 			}
 			
-		} else if (elem instanceof Solution) {
-			return;
+			descendantsCur.clear();
+			for (EObject newElem : descendantsNext) {
+				if (!descendantsAll.contains(newElem)) {
+					descendantsAll.add(newElem);
+					descendantsCur.add(newElem);
+				}
+			}
+			
+			descendantsNext.clear();
 		}
+		
+		return descendantsAll;
 	}
 }
