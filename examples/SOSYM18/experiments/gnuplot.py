f79157e60e8b40f56b2b980468893fabce3be8c5
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-03-01 18:36:55 +0100

    sosym18: Add scripts to generate gnuplot dat files

--- examples/SOSYM18/experiments/gnuplot.py
+++ examples/SOSYM18/experiments/gnuplot.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 from jproperties import Properties
 
 
-def x(path):
-    pass
+def get_values(props_path, var_x, var_y, var_line=None):
+    props = Properties()
+    with open(props_path, 'rb') as props_file:
+        props.load(props_file, 'utf-8')
+        value_x, _ = props[var_x]
+        value_line = None
+        if var_line:
+            value_line, _ = props[var_line]
+        value_y_avg, _ = props[f'{var_y}.avg']
+        value_y_avg = float(value_y_avg) / 1_000_000
+        value_y_low, _ = props[f'{var_y}.lower']
+        value_y_low = float(value_y_low) / 1_000_000
+        value_y_up, _ = props[f'{var_y}.upper']
+        value_y_up = float(value_y_up) / 1_000_000
+        return {'x': value_x, 'y': (value_y_avg, value_y_low, value_y_up), 'line': value_line}
 
 
 if __name__ == '__main__':
-    var_x = 'numSignatureSites'
-    var_y = 'timeOverhead'
-    var_line = 'ratioPolySites'
     for operator in ['filter', 'map', 'reduce']:
         data = {}
         for signature in ['model', 'rel']:
@@ -23,21 +32,12 @@ if __name__ == '__main__':
                 if not os.path.isdir(exp_path):
                     continue
                 props_path = os.path.join(exp_path, 'ExperimentOut.properties')
-                props = Properties()
-                with open(props_path, 'rb') as props_file:
-                    props.load(props_file, 'utf-8')
-                    value_x, _ = props[var_x]
-                    value_line, _ = props[var_line]
-                    value_y_avg, _ = props[f'{var_y}.avg']
-                    value_y_avg = float(value_y_avg) / 1_000_000
-                    value_y_low, _ = props[f'{var_y}.lower']
-                    value_y_low = float(value_y_low) / 1_000_000
-                    value_y_up, _ = props[f'{var_y}.upper']
-                    value_y_up = float(value_y_up) / 1_000_000
-                    x = data[signature].setdefault(value_x, {})
-                    x[value_line] = (value_y_avg, value_y_low, value_y_up)
+                values = get_values(props_path, var_x='numSignatureSites', var_y='timeOverhead',
+                                    var_line='ratioPolySites')
+                x = data[signature].setdefault(values['x'], {})
+                x[values['line']] = values['y']
         with open(f'{operator}_10types.dat', 'w') as dat_file:
-            # {'model': {0: {50: (1,2,3)}}}
+            # {'model': {100: {0.5: (1,2,3)}}}
             out = ''
             for x in sorted(data['model'].keys()):
                 if x == '0':
@@ -50,4 +50,15 @@ if __name__ == '__main__':
                     out += ' ' + ' '.join(str(n) for n in data['rel'][x][y])
                 out += '\n'
             dat_file.write(out)
+    with open('all_poly.dat', 'w') as dat_file:
+        out = ''
+        for num_types in ['1', '2', '5', '10', '20', '30']:
+            out += num_types
+            for signature in ['model', 'rel']:
+                for operator in ['filter', 'map', 'reduce']:
+                    props_path = os.path.join('poly', f'{operator}_{signature}_{num_types}', 'ExperimentOut.properties')
+                    values = get_values(props_path, var_x='numPolyTypes', var_y='timeOverhead')
+                    out += ' ' + ' '.join(str(n) for n in values['y'])
+            out += '\n'
+        dat_file.write(out)
 
