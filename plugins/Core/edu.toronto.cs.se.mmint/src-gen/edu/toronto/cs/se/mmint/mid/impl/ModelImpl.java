5902894b960f9ae948cbd3a39b449f024f5ea9d9
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-03-06 17:54:26 +0100

    operator: Add Capitalize operator for models

--- plugins/Core/edu.toronto.cs.se.mmint/src-gen/edu/toronto/cs/se/mmint/mid/impl/ModelImpl.java
+++ plugins/Core/edu.toronto.cs.se.mmint/src-gen/edu/toronto/cs/se/mmint/mid/impl/ModelImpl.java
@@ -192,7 +192,7 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public ModelOrigin getOrigin() {
-    return origin;
+    return this.origin;
   }
 
     /**
@@ -202,10 +202,10 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public void setOrigin(ModelOrigin newOrigin) {
-    ModelOrigin oldOrigin = origin;
-    origin = newOrigin == null ? ORIGIN_EDEFAULT : newOrigin;
+    ModelOrigin oldOrigin = this.origin;
+    this.origin = newOrigin == null ? ORIGIN_EDEFAULT : newOrigin;
     if (eNotificationRequired())
-      eNotify(new ENotificationImpl(this, Notification.SET, MIDPackage.MODEL__ORIGIN, oldOrigin, origin));
+      eNotify(new ENotificationImpl(this, Notification.SET, MIDPackage.MODEL__ORIGIN, oldOrigin, this.origin));
   }
 
     /**
@@ -215,7 +215,7 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public String getFileExtension() {
-    return fileExtension;
+    return this.fileExtension;
   }
 
     /**
@@ -225,10 +225,10 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public void setFileExtension(String newFileExtension) {
-    String oldFileExtension = fileExtension;
-    fileExtension = newFileExtension;
+    String oldFileExtension = this.fileExtension;
+    this.fileExtension = newFileExtension;
     if (eNotificationRequired())
-      eNotify(new ENotificationImpl(this, Notification.SET, MIDPackage.MODEL__FILE_EXTENSION, oldFileExtension, fileExtension));
+      eNotify(new ENotificationImpl(this, Notification.SET, MIDPackage.MODEL__FILE_EXTENSION, oldFileExtension, this.fileExtension));
   }
 
     /**
@@ -238,10 +238,10 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public EList<Editor> getEditors() {
-    if (editors == null) {
-      editors = new EObjectResolvingEList<Editor>(Editor.class, this, MIDPackage.MODEL__EDITORS);
+    if (this.editors == null) {
+      this.editors = new EObjectResolvingEList<>(Editor.class, this, MIDPackage.MODEL__EDITORS);
     }
-    return editors;
+    return this.editors;
   }
 
     /**
@@ -251,10 +251,10 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public EList<ModelElement> getModelElems() {
-    if (modelElems == null) {
-      modelElems = new EObjectContainmentEList<ModelElement>(ModelElement.class, this, MIDPackage.MODEL__MODEL_ELEMS);
+    if (this.modelElems == null) {
+      this.modelElems = new EObjectContainmentEList<>(ModelElement.class, this, MIDPackage.MODEL__MODEL_ELEMS);
     }
-    return modelElems;
+    return this.modelElems;
   }
 
     /**
@@ -264,10 +264,10 @@ public class ModelImpl extends GenericElementImpl implements Model {
    */
     @Override
     public EList<ConversionOperator> getConversionOperators() {
-    if (conversionOperators == null) {
-      conversionOperators = new EObjectResolvingEList<ConversionOperator>(ConversionOperator.class, this, MIDPackage.MODEL__CONVERSION_OPERATORS);
+    if (this.conversionOperators == null) {
+      this.conversionOperators = new EObjectResolvingEList<>(ConversionOperator.class, this, MIDPackage.MODEL__CONVERSION_OPERATORS);
     }
-    return conversionOperators;
+    return this.conversionOperators;
   }
 
     /**
@@ -405,15 +405,15 @@ public class ModelImpl extends GenericElementImpl implements Model {
     public boolean eIsSet(int featureID) {
     switch (featureID) {
       case MIDPackage.MODEL__ORIGIN:
-        return origin != ORIGIN_EDEFAULT;
+        return this.origin != ORIGIN_EDEFAULT;
       case MIDPackage.MODEL__FILE_EXTENSION:
-        return FILE_EXTENSION_EDEFAULT == null ? fileExtension != null : !FILE_EXTENSION_EDEFAULT.equals(fileExtension);
+        return FILE_EXTENSION_EDEFAULT == null ? this.fileExtension != null : !FILE_EXTENSION_EDEFAULT.equals(this.fileExtension);
       case MIDPackage.MODEL__EDITORS:
-        return editors != null && !editors.isEmpty();
+        return this.editors != null && !this.editors.isEmpty();
       case MIDPackage.MODEL__MODEL_ELEMS:
-        return modelElems != null && !modelElems.isEmpty();
+        return this.modelElems != null && !this.modelElems.isEmpty();
       case MIDPackage.MODEL__CONVERSION_OPERATORS:
-        return conversionOperators != null && !conversionOperators.isEmpty();
+        return this.conversionOperators != null && !this.conversionOperators.isEmpty();
     }
     return super.eIsSet(featureID);
   }
@@ -589,9 +589,9 @@ public class ModelImpl extends GenericElementImpl implements Model {
 
     StringBuilder result = new StringBuilder(super.toString());
     result.append(" (origin: ");
-    result.append(origin);
+    result.append(this.origin);
     result.append(", fileExtension: ");
-    result.append(fileExtension);
+    result.append(this.fileExtension);
     result.append(')');
     return result.toString();
   }
@@ -994,6 +994,7 @@ public class ModelImpl extends GenericElementImpl implements Model {
 
         Model newModel = this.createInstance(rootModelObj, newModelPath, instanceMID);
         if (instanceMID != null) {
+            //TODO MMINT[EDITOR] Add an optional way to select an explicit editor, to avoid creating unwanted diagrams
             newModel.createInstanceEditor(true);
         }
 
