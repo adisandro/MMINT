04db937e808b88d595f5200479c1594438ab12b0
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-04-17 16:46:38 +0200

    ocl: Convert tabs to spaces in reasoning engine

--- plugins/Core/edu.toronto.cs.se.mmint.ocl/src/edu/toronto/cs/se/mmint/ocl/reasoning/OCLReasoningEngine.java
+++ plugins/Core/edu.toronto.cs.se.mmint.ocl/src/edu/toronto/cs/se/mmint/ocl/reasoning/OCLReasoningEngine.java
@@ -33,101 +33,101 @@ import edu.toronto.cs.se.mmint.mid.relationship.ModelRel;
 
 public class OCLReasoningEngine implements IReasoningEngine {
 
-	protected final static String OCL_MODELENDPOINT_VARIABLE = "$ENDPOINT_";
-	protected final static char OCL_VARIABLE_SEPARATOR = '.';
+  protected final static String OCL_MODELENDPOINT_VARIABLE = "$ENDPOINT_";
+  protected final static char OCL_VARIABLE_SEPARATOR = '.';
 
-	protected EObject getConstraintContext(Model model, String oclConstraint, MIDLevel constraintLevel) throws MMINTException {
+  protected EObject getConstraintContext(Model model, String oclConstraint, MIDLevel constraintLevel) throws MMINTException {
 
-		//TODO MMINT[CONSTRAINT] find language to express more complex contraints on model rels
-		boolean isInstancesLevel = model.isInstancesLevel();
-		EObject modelObj = null;
-		if (model instanceof ModelRel && oclConstraint.startsWith(OCL_MODELENDPOINT_VARIABLE)) {
-			String modelEndpointConstraintName = oclConstraint.substring(OCL_MODELENDPOINT_VARIABLE.length(), oclConstraint.indexOf(OCL_VARIABLE_SEPARATOR));
-			for (ModelEndpointReference modelEndpointRef : ((ModelRel) model).getModelEndpointRefs()) {
-				//TODO MMINT[ENDPOINT] consider overridden endpoints here
-				String modelEndpointName = (isInstancesLevel && constraintLevel != MIDLevel.INSTANCES) ?
-					modelEndpointRef.getObject().getMetatype().getName() :
-					modelEndpointRef.getObject().getName();
-				if (modelEndpointConstraintName.equals(modelEndpointName)) {
-					modelObj = (isInstancesLevel) ?
-						modelEndpointRef.getObject().getTarget().getEMFInstanceRoot() :
-						modelEndpointRef.getObject().getTarget().getEMFTypeRoot();
-					break;
-				}
-			}
-			if (modelObj == null) {
-				throw new MMINTException("Can't find model endpoint " + modelEndpointConstraintName + " used in model relationship constraint");
-			}
-		}
-		else {
-			modelObj = (isInstancesLevel) ?
-				model.getEMFInstanceRoot() :
-				model.getEMFTypeRoot();
-		}
+    //TODO MMINT[CONSTRAINT] find language to express more complex contraints on model rels
+    boolean isInstancesLevel = model.isInstancesLevel();
+    EObject modelObj = null;
+    if (model instanceof ModelRel && oclConstraint.startsWith(OCL_MODELENDPOINT_VARIABLE)) {
+      String modelEndpointConstraintName = oclConstraint.substring(OCL_MODELENDPOINT_VARIABLE.length(), oclConstraint.indexOf(OCL_VARIABLE_SEPARATOR));
+      for (ModelEndpointReference modelEndpointRef : ((ModelRel) model).getModelEndpointRefs()) {
+        //TODO MMINT[ENDPOINT] consider overridden endpoints here
+        String modelEndpointName = (isInstancesLevel && constraintLevel != MIDLevel.INSTANCES) ?
+          modelEndpointRef.getObject().getMetatype().getName() :
+          modelEndpointRef.getObject().getName();
+        if (modelEndpointConstraintName.equals(modelEndpointName)) {
+          modelObj = (isInstancesLevel) ?
+            modelEndpointRef.getObject().getTarget().getEMFInstanceRoot() :
+            modelEndpointRef.getObject().getTarget().getEMFTypeRoot();
+          break;
+        }
+      }
+      if (modelObj == null) {
+        throw new MMINTException("Can't find model endpoint " + modelEndpointConstraintName + " used in model relationship constraint");
+      }
+    }
+    else {
+      modelObj = (isInstancesLevel) ?
+        model.getEMFInstanceRoot() :
+        model.getEMFTypeRoot();
+    }
 
-		return modelObj;
-	}
+    return modelObj;
+  }
 
-	@Override
-	public boolean checkModelConstraint(Model model, ExtendibleElementConstraint constraint, MIDLevel constraintLevel) {
+  @Override
+  public boolean checkModelConstraint(Model model, ExtendibleElementConstraint constraint, MIDLevel constraintLevel) {
 
-		String oclConstraint = constraint.getImplementation();
-		try {
-			EObject modelObj = getConstraintContext(model, oclConstraint, constraintLevel);
-			if (model instanceof ModelRel && oclConstraint.startsWith(OCL_MODELENDPOINT_VARIABLE)) {
-				oclConstraint = oclConstraint.substring(oclConstraint.indexOf(OCL_VARIABLE_SEPARATOR) + 1, oclConstraint.length());
-			}
-			return checkConstraint(modelObj, oclConstraint);
-		}
-		catch (MMINTException e) {
-			MMINTException.print(IStatus.ERROR, "Can't get context for OCL constraint \"" + constraint + "\" applied to model " + model + " , evaluating to false", e);
-			return false;
-		}
-	}
+    String oclConstraint = constraint.getImplementation();
+    try {
+      EObject modelObj = getConstraintContext(model, oclConstraint, constraintLevel);
+      if (model instanceof ModelRel && oclConstraint.startsWith(OCL_MODELENDPOINT_VARIABLE)) {
+        oclConstraint = oclConstraint.substring(oclConstraint.indexOf(OCL_VARIABLE_SEPARATOR) + 1, oclConstraint.length());
+      }
+      return checkConstraint(modelObj, oclConstraint);
+    }
+    catch (MMINTException e) {
+      MMINTException.print(IStatus.ERROR, "Can't get context for OCL constraint \"" + constraint + "\" applied to model " + model + " , evaluating to false", e);
+      return false;
+    }
+  }
 
-	public boolean checkConstraint(EObject modelObj, String oclConstraint) {
+  public boolean checkConstraint(EObject modelObj, String oclConstraint) {
 
-		OCL ocl = OCL.newInstance();
+    OCL ocl = OCL.newInstance();
 
-		try {
-			OCLHelper helper = ocl.createOCLHelper(modelObj.eClass());
-			ExpressionInOCL expression = helper.createInvariant(oclConstraint);
-			return ocl.check(modelObj, expression);
-		}
-		catch (Exception e) {
-			MMINTException.print(IStatus.WARNING, "Error in OCL constraint \"" + oclConstraint + "\" applied to model object " + modelObj + ", evaluating to false", e);
-			return false;
-		}
-		finally {
-			ocl.dispose();
-		}
-	}
+    try {
+      OCLHelper helper = ocl.createOCLHelper(modelObj.eClass());
+      ExpressionInOCL expression = helper.createInvariant(oclConstraint);
+      return ocl.check(modelObj, expression);
+    }
+    catch (Exception e) {
+      MMINTException.print(IStatus.WARNING, "Error in OCL constraint \"" + oclConstraint + "\" applied to model object " + modelObj + ", evaluating to false", e);
+      return false;
+    }
+    finally {
+      ocl.dispose();
+    }
+  }
 
-	@Override
-	public @Nullable Object evaluateQuery(String oclQuery, @Nullable String queryName, EObject context,
-	                                      List<@Nullable ? extends EObject> queryArguments) {
+  @Override
+  public @Nullable Object evaluateQuery(String oclQuery, @Nullable String queryName, EObject context,
+                                        List<@Nullable ? extends EObject> queryArguments) {
 
-		OCL ocl = OCL.newInstance();
+    OCL ocl = OCL.newInstance();
 
-		try {
-			OCLHelper helper = ocl.createOCLHelper(context.eClass());
-			ExpressionInOCL expression = helper.createQuery(oclQuery);
-			Object evaluation = ocl.evaluate(context, expression);
-			if (evaluation instanceof CollectionValue.Accumulator) {
-				evaluation = ((CollectionValue.Accumulator) evaluation).getElements();
-			}
-			if (evaluation instanceof SetValue) {
-				evaluation = ((SetValue) evaluation).getElements();
-			}
-			if (evaluation instanceof OrderedSetValue) {
-				evaluation = ((OrderedSetValue) evaluation).getElements();
-			}
-			return evaluation;
-		}
-		catch (Exception e) {
-			MMINTException.print(IStatus.ERROR, "OCL query error: " + oclQuery, e);
-			return null;
-		}
-	}
+    try {
+      OCLHelper helper = ocl.createOCLHelper(context.eClass());
+      ExpressionInOCL expression = helper.createQuery(oclQuery);
+      Object evaluation = ocl.evaluate(context, expression);
+      if (evaluation instanceof CollectionValue.Accumulator) {
+        evaluation = ((CollectionValue.Accumulator) evaluation).getElements();
+      }
+      if (evaluation instanceof SetValue) {
+        evaluation = ((SetValue) evaluation).getElements();
+      }
+      if (evaluation instanceof OrderedSetValue) {
+        evaluation = ((OrderedSetValue) evaluation).getElements();
+      }
+      return evaluation;
+    }
+    catch (Exception e) {
+      MMINTException.print(IStatus.ERROR, "OCL query error: " + oclQuery, e);
+      return null;
+    }
+  }
 
 }
