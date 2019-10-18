16360cd0d4ca2a4d3ff4683713c021a8675d1586
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-10-18 19:00:51 +0200

    mm: Add initial sirius diagram project part 3

--- plugins/Next/edu.toronto.cs.se.mmint3/src-gen/edu/toronto/cs/se/mmint3/mm/Element.java
+++ plugins/Next/edu.toronto.cs.se.mmint3/src-gen/edu/toronto/cs/se/mmint3/mm/Element.java
@@ -3,7 +3,9 @@
 package edu.toronto.cs.se.mmint3.mm;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.BasicEObjectImpl;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * <!-- begin-user-doc --> A representation of the model object '<em><b>Element</b></em>'. <!-- end-user-doc -->
@@ -16,12 +18,9 @@ import org.eclipse.emf.ecore.impl.BasicEObjectImpl;
 public interface Element extends EObject {
 
   /**
-   *
-   * @param <T>
-   * @return
    * @generated NOT
    */
-  default <T> T getAttribute(int featureID) {
+  private @Nullable EStructuralFeature getComposedIOrT() {
     var feature = eClass().getEStructuralFeature("i");
     if (feature == null) {
       feature = eClass().getEStructuralFeature("t");
@@ -29,26 +28,29 @@ public interface Element extends EObject {
     if (feature == null) {
       return null;
     }
-    return (T) ((BasicEObjectImpl) eGet(feature)).eGet(featureID, true, true);
-    // TODO use reflection to do getI/T().eGet(featureID)
+    return feature;
   }
 
   /**
-   *
-   * @param <T>
-   * @param value
    * @generated NOT
    */
-  default <T> void setAttribute(int featureID, T value) {
-    var feature = eClass().getEStructuralFeature("i");
+  default <T> T getAttribute(int featureID) {
+    var feature = getComposedIOrT();
     if (feature == null) {
-      feature = eClass().getEStructuralFeature("t");
+      return null;
     }
+    return (T) ((BasicEObjectImpl) eGet(feature)).eGet(featureID, true, false);
+  }
+
+  /**
+   * @generated NOT
+   */
+  default void setAttribute(int featureID, Object value) {
+    var feature = getComposedIOrT();
     if (feature == null) {
       return;
     }
     ((BasicEObjectImpl) eGet(feature)).eSet(featureID, value);
-    // TODO use reflection to do getI/T().sSet(featureID, value)
   }
 
   /**
