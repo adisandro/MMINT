f74198bdb4b52ba4362a34ab6492573e2913a563
Alessio Di Sandro <alessio.disandro@gmail.com>
2020-01-14 21:31:24 +0100

    mm: Add preliminary command to import models

---
+++ plugins/Next/edu.toronto.cs.se.mmint3.design/src/edu/toronto/cs/se/mmint3/design/actions/ModelInstanceImport.java
@@ -0,0 +1,28 @@
+package edu.toronto.cs.se.mmint3.design.actions;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.business.api.action.AbstractExternalJavaAction;
+
+import edu.toronto.cs.se.mmint3.MMTypes;
+import edu.toronto.cs.se.mmint3.mm.InstanceMegaModel;
+import edu.toronto.cs.se.mmint3.mm.ModelType;
+
+public class ModelInstanceImport extends AbstractExternalJavaAction {
+
+  @Override
+  public boolean canExecute(Collection<? extends EObject> arg0) {
+    return true;
+  }
+
+  @Override
+  public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
+    var mm = (InstanceMegaModel) arg1.get("mm");
+    ModelType modelT = MMTypes.getType("edu.toronto.cs.se.mmint3.mm");
+    var modelPath = "/next/model.xmi";
+    var model = modelT.createInstance(null, modelPath, false, mm);
+  }
+
+}
