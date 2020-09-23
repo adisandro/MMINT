66961189b10e7fb3ab546bf77b109d535cc81370
Alessio Di Sandro <alessio.disandro@gmail.com>
2020-09-23 02:30:18 +0200

    lean: Create a simple implementation of the model constraint trait

--- plugins/External/Lean/edu.toronto.cs.se.mmint.lean/src/edu/toronto/cs/se/mmint/lean/reasoning/LeanReasoner.java
+++ plugins/External/Lean/edu.toronto.cs.se.mmint.lean/src/edu/toronto/cs/se/mmint/lean/reasoning/LeanReasoner.java
@@ -9,6 +9,11 @@
  */
 package edu.toronto.cs.se.mmint.lean.reasoning;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import edu.toronto.cs.se.mmint.mid.ExtendibleElementConstraint;
 import edu.toronto.cs.se.mmint.mid.MIDLevel;
 import edu.toronto.cs.se.mmint.mid.Model;
@@ -16,9 +21,30 @@ import edu.toronto.cs.se.mmint.mid.reasoning.IModelConstraintTrait;
 
 public class LeanReasoner implements IModelConstraintTrait {
 
+  public final static String EXECUTABLE = "/path/to/lean";
+  public final static String FILE = "/path/to/lean/project/temp.lean";
+
   @Override
-  public boolean checkModelConstraint(Model model, ExtendibleElementConstraint constraint, MIDLevel constraintLevel) {
-    // TODO Auto-generated method stub
+  public boolean checkModelConstraint(Model model, ExtendibleElementConstraint constraint, MIDLevel constraintLevel)
+                                     throws Exception {
+    Files.writeString(Paths.get(LeanReasoner.FILE), constraint.getImplementation());
+    var builder = new ProcessBuilder(LeanReasoner.EXECUTABLE, LeanReasoner.FILE);
+    var process = builder.start();
+    var output = new StringBuilder();
+    try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        output.append(line + System.lineSeparator());
+      }
+    }
+    var result = process.waitFor();
+    if (result != 0) {
+      return false;
+    }
+    if (output.toString().trim().equals("tt")) {
+      return true;
+    }
+
     return false;
   }
 
