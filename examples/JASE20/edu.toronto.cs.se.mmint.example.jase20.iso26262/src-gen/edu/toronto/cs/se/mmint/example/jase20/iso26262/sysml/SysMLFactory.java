4f2218b4b340ef356104bfbbbb24ac2e9081a578
Alessio Di Sandro <alessio.disandro@gmail.com>
2020-10-27 01:27:48 +0100

    jase20: Reorganize copyright notices

--- examples/JASE20/edu.toronto.cs.se.mmint.example.jase20.iso26262/src-gen/edu/toronto/cs/se/mmint/example/jase20/iso26262/sysml/SysMLFactory.java
+++ examples/JASE20/edu.toronto.cs.se.mmint.example.jase20.iso26262/src-gen/edu/toronto/cs/se/mmint/example/jase20/iso26262/sysml/SysMLFactory.java
@@ -1,12 +1,15 @@
-/**
- * Copyright (c) 2012-2020 Alessio Di Sandro, Marsha Chechik, Rick Salay, Sahar Kokaly.
- * All rights reserved. This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
+/*******************************************************************************
+ * Copyright (c) 2012, 2020 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
  * Contributors:
- *   Alessio Di Sandro - Implementation
- */
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
 package edu.toronto.cs.se.mmint.example.jase20.iso26262.sysml;
 
 import org.eclipse.emf.ecore.EFactory;
