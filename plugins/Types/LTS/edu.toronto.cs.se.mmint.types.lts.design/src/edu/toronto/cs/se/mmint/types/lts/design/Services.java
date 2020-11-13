d7f9d1c7b76258eb673c6481eb2c7d891bc7f061
Alessio Di Sandro <alessio.disandro@gmail.com>
2020-11-13 03:21:04 +0100

    lts: Add LTS model diagram

---
+++ plugins/Types/LTS/edu.toronto.cs.se.mmint.types.lts.design/src/edu/toronto/cs/se/mmint/types/lts/design/Services.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
+package edu.toronto.cs.se.mmint.types.lts.design;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * The services class used by VSM.
+ */
+public class Services {
+
+    /**
+    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
+    */
+    public EObject myService(EObject self, String arg) {
+       // TODO Auto-generated code
+      return self;
+    }
+}
