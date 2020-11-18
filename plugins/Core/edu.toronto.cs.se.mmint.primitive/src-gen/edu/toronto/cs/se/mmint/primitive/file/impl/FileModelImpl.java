62c72c05ca078f899b0b56128c9478db64cd6c8a
Alessio Di Sandro <alessio.disandro@gmail.com>
2020-11-18 03:37:38 +0100

    core: Add file extension to model name when creating a file model

--- plugins/Core/edu.toronto.cs.se.mmint.primitive/src-gen/edu/toronto/cs/se/mmint/primitive/file/impl/FileModelImpl.java
+++ plugins/Core/edu.toronto.cs.se.mmint.primitive/src-gen/edu/toronto/cs/se/mmint/primitive/file/impl/FileModelImpl.java
@@ -5,24 +5,29 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Alessio Di Sandro - Implementation.
  */
 package edu.toronto.cs.se.mmint.primitive.file.impl;
 
-import edu.toronto.cs.se.mmint.MMINTException;
-import edu.toronto.cs.se.mmint.mid.impl.ModelImpl;
-import edu.toronto.cs.se.mmint.primitive.file.FileModel;
-import edu.toronto.cs.se.mmint.primitive.file.FilePackage;
+import java.io.IOException;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
+
+import edu.toronto.cs.se.mmint.MMINTException;
+import edu.toronto.cs.se.mmint.mid.MID;
+import edu.toronto.cs.se.mmint.mid.Model;
+import edu.toronto.cs.se.mmint.mid.impl.ModelImpl;
+import edu.toronto.cs.se.mmint.mid.utils.FileUtils;
+import edu.toronto.cs.se.mmint.primitive.file.FileModel;
+import edu.toronto.cs.se.mmint.primitive.file.FilePackage;
 
 /**
  * <!-- begin-user-doc -->
@@ -60,8 +65,20 @@ public class FileModelImpl extends ModelImpl implements FileModel {
 		MMINTException.mustBeInstance(this);
 
 		IPath path = new Path(this.getUri());
-		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+		var file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
 		IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), file);
 	}
+
+  /**
+   * @generated NOT
+   */
+  @Override
+  public Model createInstance(EObject rootModelObj, String newModelPath, MID instanceMID) throws MMINTException,
+                                                                                                 IOException {
+    var newModel = super.createInstance(rootModelObj, newModelPath, instanceMID);
+    newModel.setName(FileUtils.getLastSegmentFromPath(newModelPath));
+
+    return newModel;
+  }
 
 } //FileModelImpl
