d850fd8fbb3e03acad4a74c8ba21f0d71b344e7c
Alessio Di Sandro <alessio.disandro@gmail.com>
2021-06-08 12:12:14 +0200

    models15: Convert ocl constraint to java to speed it up

---
+++ examples/MODELS15/edu.toronto.cs.se.modelepedia.models15/src/edu/toronto/cs/se/modelepedia/models15/operator/CDNoPublicAttributes.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2021, 2021 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
+package edu.toronto.cs.se.modelepedia.models15.operator;
+
+import edu.toronto.cs.se.mmint.mid.Model;
+
+public class CDNoPublicAttributes extends CDPublicAttributes {
+
+  @Override
+  public boolean check(Model model) {
+    return !super.check(model);
+  }
+}
---
+++ examples/MODELS15/edu.toronto.cs.se.modelepedia.models15/src/edu/toronto/cs/se/modelepedia/models15/operator/CDPublicAttributes.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2021, 2021 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
+package edu.toronto.cs.se.modelepedia.models15.operator;
+
+import edu.toronto.cs.se.mmint.java.reasoning.IJavaModelConstraint;
+import edu.toronto.cs.se.mmint.mid.Model;
+import edu.toronto.cs.se.modelepedia.classdiagram.ClassDiagram;
+
+public class CDPublicAttributes implements IJavaModelConstraint {
+
+  @Override
+  public boolean check(Model model) {
+    var cdRoot = (ClassDiagram) model.getEMFInstanceRoot();
+    return cdRoot.getClasses().stream()
+      .flatMap(c -> c.getOwnedAttributes().stream())
+      .anyMatch(a -> a.isPublic());
+  }
+}
