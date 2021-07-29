93f3922a5d36c818cd7454cd6a3dbbe56c0fcb17
Alessio Di Sandro <alessio.disandro@gmail.com>
2021-07-29 17:31:26 +0200

    product-line: Add conversion from a product line to a model part 1

---
+++ plugins/Core-Extensions/ProductLine/edu.toronto.cs.se.mmint.productline.operators/src/edu/toronto/cs/se/mmint/productline/operators/ToProduct.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (c) 2021, 2021 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
+package edu.toronto.cs.se.mmint.productline.operators;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.ecore.EObject;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.TimeoutException;
+
+import edu.toronto.cs.se.mmint.MIDTypeRegistry;
+import edu.toronto.cs.se.mmint.MMINTException;
+import edu.toronto.cs.se.mmint.mid.GenericElement;
+import edu.toronto.cs.se.mmint.mid.MID;
+import edu.toronto.cs.se.mmint.mid.Model;
+import edu.toronto.cs.se.mmint.mid.operator.impl.OperatorImpl;
+import edu.toronto.cs.se.mmint.productline.ProductLine;
+
+public class ToProduct extends OperatorImpl {
+  private Input input;
+  private Output output;
+
+  private static class Input {
+    public final static String MODEL = "productLine";
+    public Model plModel;
+    public ProductLine pl;
+
+    public Input(Map<String, Model> inputsByName) {
+      this.plModel = inputsByName.get(Input.MODEL);
+      this.pl = (ProductLine) this.plModel.getEMFInstanceRoot();
+    }
+  }
+
+  private static class Output {
+    public final static String MODEL = "product";
+    public Model productModelType;
+    public EObject product;
+    public String productPath;
+    public MID mid;
+
+    public Output(Map<String, MID> outputMIDsByName, String workingPath, Input input) {
+      this.productModelType = MIDTypeRegistry.<Model>getType(input.pl.getMetamodel().getNsURI());
+      this.productPath = workingPath + IPath.SEPARATOR + input.plModel.getName() + "." +
+                         this.productModelType.getFileExtension();
+      this.mid = outputMIDsByName.get(Output.MODEL);
+    }
+
+    public Map<String, Model> packed() throws MMINTException, IOException {
+      var plModel = this.productModelType.createInstanceAndEditor(this.product, this.productPath, this.mid);
+
+      return Map.of(Output.MODEL, plModel);
+    }
+  }
+
+  private void init(Map<String, Model> inputsByName, Map<String, MID> outputMIDsByName) throws Exception {
+    this.input = new Input(inputsByName);
+    this.output = new Output(outputMIDsByName, getWorkingPath(), this.input);
+  }
+
+  private void toProduct() throws TimeoutException {
+    //TODO ask for variable values, check that getFeatures is sat, then check vars with each presence condition
+    var productModelObjs = new HashMap<String, EObject>();
+    var productFactory = this.input.pl.getMetamodel().getEFactoryInstance();
+    var solver = SolverFactory.newLight();
+    solver.setTimeout(60);
+    for (var plClass : this.input.pl.getClasses()) {
+      IProblem problem = solver;
+      if (problem.isSatisfiable()) {
+        var productModelObj = productFactory.create(plClass.getType());
+        for (var plAttribute : plClass.getAttributes()) {
+
+        }
+      }
+    }
+    for (var plReference : this.input.pl.getReferences()) {
+
+    }
+  }
+
+  @Override
+  public Map<String, Model> run(Map<String, Model> inputsByName, Map<String, GenericElement> genericsByName,
+                                Map<String, MID> outputMIDsByName) throws Exception {
+    init(inputsByName, outputMIDsByName);
+    toProduct();
+
+    return this.output.packed();
+  }
+
+}
