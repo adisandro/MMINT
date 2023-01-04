9215ac4012e20d52e252f275fb94939859c046c4
Alessio Di Sandro <alessio.disandro@gmail.com>
2023-01-04 18:09:26 +0100

    releng: Update copyright to 2023

--- plugins/Types/GSN/edu.toronto.cs.se.modelepedia.gsn.edit/src-gen/edu/toronto/cs/se/modelepedia/gsn/provider/StrategyItemProvider.java
+++ plugins/Types/GSN/edu.toronto.cs.se.modelepedia.gsn.edit/src-gen/edu/toronto/cs/se/modelepedia/gsn/provider/StrategyItemProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2022 Alessio Di Sandro, Nick Fung.
+ * Copyright (c) 2017, 2023 Alessio Di Sandro, Nick Fung.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
