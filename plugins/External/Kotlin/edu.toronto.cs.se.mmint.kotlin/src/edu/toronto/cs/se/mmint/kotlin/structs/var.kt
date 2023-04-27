6a4f029824da010b1dddc3e1e9283118eb56d48a
loganrjmurphy <loganrjmurphy@gmail.com>
2023-04-27 15:16:30 -0400

    with inheritance

--- plugins/External/Kotlin/edu.toronto.cs.se.mmint.kotlin/src/edu/toronto/cs/se/mmint/kotlin/structs/var.kt
+++ plugins/External/Kotlin/edu.toronto.cs.se.mmint.kotlin/src/edu/toronto/cs/se/mmint/kotlin/structs/var.kt
@@ -13,137 +13,73 @@
  *******************************************************************************/
 package edu.toronto.cs.se.mmint.kotlin.structs
 
-//sealed class PC() {
-//    override fun toString(): String {
-//        when(this) {
-//            is Atom -> return this.a
-//            is Or ->  {
-//                val left = this.a
-//                val right = this.b
-//                return "$left | $right"
-//            }
-//        }
-//    }
-//}
+
+import edu.toronto.cs.se.mmint.kotlin.operators.merge.mergeAttrs
+import edu.toronto.cs.se.mmint.kotlin.operators.merge.mergeMaps
+
 //
-//data class Atom(val a : String) : PC()
-//data class Or(val a: PC, val b : PC) : PC() {
-//    override fun toString(): String {
-//        return "$a | $b"
-//    }
-//}
-
-sealed class Var<out a> {
-    override fun toString(): String {
-        return when (this) {
-            is MkVar -> this.x.toString()
-        }
-    }
-}
-
-
-data class MkVar<out a> (val pcond : String, val x : a) : Var<a>()
-
-//fun <a> Var<a>.pc() : PC =
-//    when (this){
-//        is MkVar -> this.pcond
-//    }
-
-
-sealed class VarObj {
-    override fun toString(): String {
-        return when(this) {
-            is MkVarObj -> this.uri
-        }
-    }
-}
-//data class MkVarObj(val pcond : PC, val x : Object) : VarObj() {
-//    override fun toString(): String {
-//        return this.x.toString()
-//    }
-//
-//    override fun equals(other: Any?): Boolean
-//    = other is MkVarObj && other.x == x
-//
-//}
-
-data class MkVarObj(val pcond : String, val uri : String, val kind: String, val attrs: MutableMap<String, String>, val refs: MutableMap<String, LList<Tree<VarObj>>>) : VarObj() {
-    override fun toString(): String = uri
-    override fun equals(other: Any?): Boolean
-            = other is MkVarObj && uri == other.uri
-}
-
-fun <a> Var<a>.pc() : String = when (this) { is MkVar -> this.pcond }
-fun <a> Var<a>.value() : a = when (this) { is MkVar -> this.x }
 
 fun Or(left : String, right : String) : String = "$left | $right"
 
 
-fun VarObj.pc() : String = when (this) {is MkVarObj -> this.pcond}
-
-fun VarObj.uri() : String = when (this) { is MkVarObj -> this.uri}
-
-fun VarObj.kind() : String =
-    when (this) {
-        is MkVarObj -> this.kind
+data class VarObj(val pcond : String,
+                   override val uri: String,
+                   override val kind: String,
+                   override val attrs : Map<String,String>,
+                   override val refs: MutableMap<String, LList<Tree<Object>>>
+                  ) :
+                  Object() {
+    override fun toString(): String {
+        return uri
     }
-
-fun VarObj.attrs() : MutableMap<String, String> =
-    when (this) {
-        is MkVarObj -> this.attrs
+    companion object {
+        fun unionPL(
+            toMerge: Map<String, String>,
+            mergedObjs: LList<Prod<String, String>>,
+            left: VarObj,
+            right: VarObj
+        ): VarObj {
+            println("here")
+            val mergedURI = when (val o = mergedObjs.lookup(left.uri)) {
+                is None -> left.uri
+                is Some -> o.x
+            }
+            val mergedKind = if (left.kind == right.kind) left.kind else "KIND_ERROR"
+            val mergedAttrs = mergeAttrs(left.attrs, right.attrs)
+            val mergedRefs = mergeMaps(left.refs, right.refs, toMerge)
+            return VarObj(
+                Or(left.pcond, right.pcond),
+                mergedURI, mergedKind, mergedAttrs, mergedRefs
+            )
+        }
     }
-
-fun VarObj.refsPL() : MutableMap<String, LList<Tree<VarObj>>> =
-    when (this) {
-        is MkVarObj -> this.refs
-    }
-
-fun VarObj.getAttr(s : String) : String? =
-    when (this) {
-        is MkVarObj -> this.attrs[s]
-    }
-
-fun VarObj.setRefsPL(map: MutableMap<String,LList<Tree<VarObj>>>) {
-    this.refsPL().putAll(map)
 }
 
-fun VarObj.toObject() : Object {
-    val attrs = this.attrs()
-    attrs["presenceCondition"] = this.pc()
-    return MkObject(
-        this.uri(),
-        this.kind(),
-        attrs,
-        this.refsPL().toLList().map { p -> MkProd(p.fst(), p.snd().map { it.mapdata { x -> x.toObject() } }) }.toMap()
-            .toMutableMap())
-}
+
 
 fun Object.toVarObj() : VarObj {
-    when(this){
-        is MkObject ->
-            if (this.attrs().containsKey("presenceCondition")){
-                val pc = this.attrs()["presenceCondition"]
+
+            if (this.attrs.containsKey("presenceCondition")){
+                val pc = this.attrs["presenceCondition"]
                 return when(pc) {
-                    null -> MkVarObj("true", this.uri(), this.kind(), this.attrs().toMutableMap(),this.refs().toLList().map { p -> MkProd(p.fst(), p.snd().map {it.mapdata { x -> x.toVarObj() }})} .toMap().toMutableMap())
+                    null -> VarObj("true", uri, kind, attrs.minus("presenceCondition").toMutableMap(),
+                        refs.toLList().map { p -> MkProd(p.fst(), p.snd().map {it.mapdata { x -> x.toVarObj() }})} .toMap().toMutableMap())
                     else -> {
-                        this.attrs().minus("presenceCondition")
-                        MkVarObj(pc, this.uri(), this.kind(), this.attrs().toMutableMap(),this.refs().toLList().map {p -> MkProd(p.fst(), p.snd().map {it.mapdata { x -> x.toVarObj() }})} .toMap().toMutableMap())
+                        this.attrs.minus("presenceCondition")
+                        VarObj(pc, uri, kind, attrs.minus("presenceCondition").toMutableMap(),
+                            refs.toLList().map { p -> MkProd(p.fst(), p.snd().map {it.mapdata { x -> x.toVarObj() }})} .toMap().toMutableMap())
                     }
                 }
 
             }
         else {
-               return MkVarObj("true", this.uri(), this.kind(), this.attrs().toMutableMap(),this.refs().toLList().map { p -> MkProd(p.fst(), p.snd().map {it.mapdata { x -> x.toVarObj() }})} .toMap().toMutableMap())
+               return VarObj("true", uri, kind, attrs.minus("presenceCondition").toMutableMap(),
+                   refs.toLList().map { p -> MkProd(p.fst(), p.snd().map {it.mapdata { x -> x.toVarObj() }})} .toMap().toMutableMap())
             }
     }
-}
 //
-//fun toVarObject(pc : String, obj : Object) : VarObj =
-//    MkVarObj(pc, obj.uri(), obj.kind(), obj.attrs().toMutableMap(), obj.refs().toMutableMap())
-
-fun Tree<VarObj>.toTreeObj() : Tree<Object> =
-    this.mapdata { it.toObject() }
 
 fun Tree<Object>.toTreeVarObj() : Tree<VarObj> =
     this.mapdata { it.toVarObj()}
+
 
