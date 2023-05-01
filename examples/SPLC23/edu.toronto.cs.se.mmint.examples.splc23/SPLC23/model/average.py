e36a86993b2a1152b1af3310a79a3de6653bf3f6
Alessio Di Sandro <alessio.disandro@gmail.com>
2023-05-01 04:58:15 +0200

    splc23: Add result parsing scripts

---
+++ examples/SPLC23/edu.toronto.cs.se.mmint.examples.splc23/SPLC23/model/average.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def read_result(file):
+    time_merge = 0
+    for line in file.readlines():
+        match line.partition('='):
+            case ('Merge.timeMerge.avg', eq, val):
+                time_merge = float(val)
+            case _:
+                pass
+    return time_merge
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('model_name')
+    args = parser.parse_args()
+    SIZES = ['orig', '1k', '10k']
+    IDS = ['J10', 'K10', 'J100', 'K100', 'J500', 'K500']
+    PRODUCTS = {'GPL': 840, 'Automata': 2016, 'bCMS-SPL-PC': 15360, 'crom_l1': 2395000}
+    for size in SIZES:
+        latex = ''
+        for id in IDS:
+            print(f'{args.model_name} {size}_{id}:')
+            time_merge_p = 0
+            time_merge_pl = 0
+            with open(f'{args.model_name}/results/P_{size}_{id}.properties') as result_file:
+                result = read_result(result_file)
+                time_merge_p = result
+            with open(f'{args.model_name}/results/PL_{size}_{id}.properties') as result_file:
+                result = read_result(result_file)
+                time_merge_pl = result
+            speedup = round(time_merge_p * PRODUCTS[args.model_name] / time_merge_pl)
+            print(f'  Speedup: {speedup}')
+            latex += f' & {speedup}'
+        print(f'Latex {args.model_name} {size}: {latex}')
+
---
+++ examples/SPLC23/edu.toronto.cs.se.mmint.examples.splc23/SPLC23/model/collect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import shutil
+from argparse import ArgumentParser
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('model_name')
+    parser.add_argument('p_or_pl')
+    parser.add_argument('size')
+    args = parser.parse_args()
+    IDS = ['J10', 'J100', 'J500', 'K10', 'K100', 'K500']
+    for i, id in enumerate(IDS):
+        shutil.move(f'{args.model_name}/experiment{i}/ExperimentOut.properties',
+                    f'{args.model_name}/results/{args.p_or_pl}_{args.size}_{id}.properties')
+        shutil.rmtree(f'{args.model_name}/experiment{i}')
+
