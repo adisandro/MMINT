fd4ec8ebae341c0baccbb27c2393225694b4d345
Alessio Di Sandro <alessio.disandro@gmail.com>
2023-05-02 03:05:32 +0200

    splc23: Update result scripts

--- examples/SPLC23/edu.toronto.cs.se.mmint.examples.splc23/SPLC23/model/average.py
+++ examples/SPLC23/edu.toronto.cs.se.mmint.examples.splc23/SPLC23/model/average.py
@@ -19,23 +19,23 @@ if __name__ == "__main__":
     parser.add_argument('model_name')
     args = parser.parse_args()
     SIZES = ['orig', '1k', '5k']
-    IDS = ['J10', 'K10', 'J100', 'K100', 'J500', 'K500']
+    IDS = ['10', '100', '500']
     PRODUCTS = {'GPL': 840, 'Automata': 2016, 'bCMS-SPL-PC': 15360, 'crom_l1': 2395000}
     for size in SIZES:
         latex = ''
         for id in IDS:
             try:
-                with open(f'{args.model_name}/results/P_{size}_{id}.properties') as result_file:
+                with open(f'{args.model_name}/results/P_{size}_J{id}.properties') as result_file:
                     result = read_result(result_file)
                     time_merge_p = result
-                with open(f'{args.model_name}/results/PL_{size}_{id}.properties') as result_file:
+                with open(f'{args.model_name}/results/PL_{size}_K{id}.properties') as result_file:
                     result = read_result(result_file)
                     time_merge_pl = result
                 speedup = round(time_merge_p * PRODUCTS[args.model_name] / time_merge_pl, 2)
                 if speedup < 1:
                     speedup = - round(time_merge_pl / (time_merge_p * PRODUCTS[args.model_name]), 2)
                 print(f'{args.model_name} {size}_{id}:')
-                print(f'  Speedup: {speedup}')
+                print(f'  K/J Speedup: {speedup}')
                 latex += f' & {speedup}'
             except:
                 latex += f' & -'
