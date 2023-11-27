d56185f2b31a6e11d2a4babcf673144737a942b4
Alessio Di Sandro <alessio.disandro@gmail.com>
2023-11-28 00:23:33 +0100

    gsn+pl: Implement GSN PL editor part 3

--- plugins/Types/GSN/edu.toronto.cs.se.mmint.types.gsn.productline.design/src/edu/toronto/cs/se/mmint/types/gsn/productline/design/GSNProductLineServices.java
+++ plugins/Types/GSN/edu.toronto.cs.se.mmint.types.gsn.productline.design/src/edu/toronto/cs/se/mmint/types/gsn/productline/design/GSNProductLineServices.java
@@ -39,6 +39,7 @@ public class GSNProductLineServices extends ProductLineServices {
         }
         yield id + " " + desc;
       }
+      case Class c -> "";
       default -> getElementLabel(self);
     };
   }
