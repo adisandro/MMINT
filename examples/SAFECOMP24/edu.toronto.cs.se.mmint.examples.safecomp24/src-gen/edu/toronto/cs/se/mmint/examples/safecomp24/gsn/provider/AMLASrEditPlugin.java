d8e025eaee19e59944b639bf28028d8b688f7af4
Alessio Di Sandro <alessio.disandro@gmail.com>
2024-02-08 18:00:37 +0100

    safecomp24: Create custom template part 2

---
+++ examples/SAFECOMP24/edu.toronto.cs.se.mmint.examples.safecomp24/src-gen/edu/toronto/cs/se/mmint/examples/safecomp24/gsn/provider/AMLASrEditPlugin.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2024, 2024 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
+package edu.toronto.cs.se.mmint.examples.safecomp24.gsn.provider;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import edu.toronto.cs.se.modelepedia.gsn.provider.GSNEditPlugin;
+
+/**
+ * This is the central singleton for the AMLASr edit plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class AMLASrEditPlugin extends EMFPlugin {
+  /**
+   * Keep track of the singleton.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static final AMLASrEditPlugin INSTANCE = new AMLASrEditPlugin();
+
+  /**
+   * Keep track of the singleton.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  private static Implementation plugin;
+
+  /**
+   * Create the instance.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public AMLASrEditPlugin() {
+    super
+      (new ResourceLocator [] {
+         GSNEditPlugin.INSTANCE,
+       });
+  }
+
+  /**
+   * Returns the singleton instance of the Eclipse plugin.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the singleton instance.
+   * @generated
+   */
+  @Override
+  public ResourceLocator getPluginResourceLocator() {
+    return AMLASrEditPlugin.plugin;
+  }
+
+  /**
+   * Returns the singleton instance of the Eclipse plugin.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the singleton instance.
+   * @generated
+   */
+  public static Implementation getPlugin() {
+    return AMLASrEditPlugin.plugin;
+  }
+
+  /**
+   * The actual implementation of the Eclipse <b>Plugin</b>.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static class Implementation extends EclipsePlugin {
+    /**
+     * Creates an instance.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public Implementation() {
+      super();
+
+      // Remember the static instance.
+      //
+      AMLASrEditPlugin.plugin = this;
+    }
+  }
+
+}
