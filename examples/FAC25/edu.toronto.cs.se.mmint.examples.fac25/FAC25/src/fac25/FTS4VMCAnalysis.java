05079eee3c03db6b5a2febeb81d8248977065b40
Alessio Di Sandro <alessio.disandro@gmail.com>
2025-03-18 18:29:35 +0100

    fac25: Add infusion pump models part 5

---
+++ examples/FAC25/edu.toronto.cs.se.mmint.examples.fac25/FAC25/src/fac25/FTS4VMCAnalysis.java
@@ -0,0 +1,243 @@
+/*******************************************************************************
+ * Copyright (c) 2024, 2025 Alessio Di Sandro.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alessio Di Sandro - Implementation
+ *******************************************************************************/
+package fac25;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.ECollections;
+
+import edu.toronto.cs.se.mmint.MMINTException;
+import edu.toronto.cs.se.mmint.mid.ui.MIDDialogs;
+import edu.toronto.cs.se.mmint.mid.utils.FileUtils;
+import edu.toronto.cs.se.mmint.productline.ProductLine;
+import edu.toronto.cs.se.mmint.types.gsn.productline.PLGSNAnalyticTemplate;
+import edu.toronto.cs.se.mmint.types.gsn.productline.PLGSNArgumentElement;
+import edu.toronto.cs.se.mmint.types.gsn.productline.PLGSNTemplate;
+import edu.toronto.cs.se.mmint.types.gsn.productline.reasoning.IPLGSNAnalysis;
+import edu.toronto.cs.se.mmint.types.gsn.productline.util.PLGSNBuilder;
+import edu.toronto.cs.se.mmint.types.gsn.productline.util.PLGSNChangeStep;
+import edu.toronto.cs.se.mmint.types.gsn.templates.GSNTemplatesPackage;
+import edu.toronto.cs.se.modelepedia.gsn.GSNPackage;
+import edu.toronto.cs.se.modelepedia.gsn.ImpactType;
+import edu.toronto.cs.se.modelepedia.gsn.util.ChangeStep;
+import edu.toronto.cs.se.modelepedia.statemachine.StateMachinePackage;
+
+/**
+ * This template is producing evidence based on checking a property over the underlying model with the FTS4VMC model
+ * checker. It is a leaf template, with no downstream supporters.
+ */
+public class FTS4VMCAnalysis implements IPLGSNAnalysis {
+  protected final static String PROPERTY_FILE = "property.txt";
+  protected final static String RUN_FILE = "run.sh";
+  protected final static String SAT_FILE = "result.out";
+  protected GSNPackage gsn;
+
+  public FTS4VMCAnalysis() {
+    this.gsn = GSNPackage.eINSTANCE;
+  }
+
+  private String presenceCondition2Dot(String pc) {
+    return pc.replace("&", "and").replace("|", "or").replace("~", "not ").replace("$true", "True");
+  }
+
+  protected String productLine2Dot(ProductLine productLine, String name) {
+    var types = StateMachinePackage.eINSTANCE;
+    var out = "digraph " + name + "{";
+    out += "\n  name=\"" + name + "\"";
+    var fm = presenceCondition2Dot(productLine.getFeaturesConstraint());
+    out += "\n  FM=\"" + fm + "\"";
+    for (var s : productLine.getClasses()) {
+      if (s.instanceOf(types.getInitialState())) {
+        out += "\n  " + s.getAttribute(types.getAbstractState_Name()).get(0) + "[initial=True]";
+        break;
+      }
+    }
+    for (var t : productLine.getClasses()) {
+      if (!t.instanceOf(types.getTransition())) {
+        continue;
+      }
+      for (var src : t.getReference(types.getTransition_Source())) {
+        var srcName = src.getAttribute(types.getAbstractState_Name()).get(0);
+        for (var tgt : t.getReference(types.getTransition_Target())) {
+          var tgtName = tgt.getAttribute(types.getAbstractState_Name()).get(0);
+          out += "\n  " + srcName + " -> " + tgtName + "[label=\"" + tgtName + " | " +
+                 presenceCondition2Dot(t.getPresenceCondition()) + "\"];";
+        }
+      }
+    }
+    out += "\n}";
+
+    return out;
+  }
+
+  protected String runFTS4VMC(String modelPath, String propertyPath) throws Exception {
+    var modelPL = (ProductLine) FileUtils.readModelFile(modelPath, null, false);
+    if (modelPL.getMetamodel() != StateMachinePackage.eINSTANCE) {
+      throw new MMINTException("Model type '" + modelPL.getMetamodel().getNsURI() + "' not supported");
+    }
+    var modelName = FileUtils.getFileNameFromPath(modelPath);
+    var dot = productLine2Dot(modelPL, modelName);
+    var dotPath = FileUtils.replaceFileExtensionInPath(modelPath, "dot");
+    var vmcPath = FileUtils.replaceFileExtensionInPath(modelPath, "vmc");
+    FileUtils.createTextFile(dotPath, dot, false);
+    final var RUN_SH = """
+      python3 -m venv venv
+      source venv/bin/activate
+      git clone https://github.com/fts4vmc/FTS4VMC.git &> /dev/null
+      pip3 install -r FTS4VMC/requirements.txt &> /dev/null
+      pip3 install --upgrade z3-solver &> /dev/null
+      python3 FTS4VMC/translate.py\s""" + dotPath + " " +  vmcPath + " &> /dev/null\n" +
+      "FTS4VMC/vmc65-linux " + vmcPath + " " + propertyPath;
+    var runPath = Paths.get(FileUtils.replaceLastSegmentInPath(modelPath, FTS4VMCAnalysis.RUN_FILE));
+    Files.writeString(runPath, RUN_SH);
+    var result = FileUtils.runShell(runPath.getParent().toString(), "bash", FTS4VMCAnalysis.RUN_FILE);
+
+    return result;
+  }
+
+  @Override
+  public void import_(PLGSNAnalyticTemplate plTemplate, ProductLine productLine) throws Exception {
+    var builder = new PLGSNBuilder(productLine);
+    var mcStrategy = plTemplate.getElementsById().get("mcStrategy");
+    var desc = mcStrategy.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+      .replace("model checking", "lifted model checking");
+    mcStrategy.setAttribute(this.gsn.getArgumentElement_Description(), desc);
+    var liftedGoal = builder.createGoal("G4", "The lifted model checker is correct", null);
+    liftedGoal.addAttribute(this.gsn.getArgumentElement_TemplateId(), "liftedGoal");
+    plTemplate.addReference(this.gsn.getTemplate_Elements(), liftedGoal);
+    builder.addSupporter(mcStrategy, liftedGoal);
+  }
+
+  @Override
+  public void instantiate(PLGSNAnalyticTemplate plTemplate) throws Exception {
+    var templateElems = plTemplate.getElementsById();
+    // select PL model and property
+    String modelPath = null;
+    String dialogInitial = null;
+    var safetyGoal = templateElems.get("safetyGoal");
+    if (safetyGoal == null) {
+      safetyGoal = (PLGSNArgumentElement) templateElems.get("mcStrategy")
+        .getReference(this.gsn.getSupporter_Supports()).get(0)
+        .getReference(this.gsn.getSupportedBy_Source()).get(0);
+      var otherTemplate = safetyGoal.getReference(this.gsn.getArgumentElement_Template()).get(0);
+      if (otherTemplate.getAttribute(this.gsn.getTemplate_Id()).get(0).equals("QueryAnalysis")) {
+        // connected with query analysis template, extract model from it
+        var otherFilesCtx = ((PLGSNTemplate) otherTemplate).getElementsById().get("filesCtx");
+        var paths = otherFilesCtx.getManyAttribute(GSNTemplatesPackage.eINSTANCE.getFilesContext_Paths()).get(0);
+        modelPath = paths.get(1);
+        dialogInitial = safetyGoal.getAttribute(this.gsn.getArgumentElement_Description()).get(0).split("'")[1];
+      }
+    }
+    if (modelPath == null) {
+      modelPath = FileUtils.prependWorkspacePath(
+        MIDDialogs.selectFile("Run Product Line analysis", "Select a Product Line model",
+                              "There are no Product Line models in the workspace", Set.of("productline")));
+    }
+    var property = MIDDialogs.getBigStringInput("Run Product Line analysis", "Insert model property to check",
+                                                dialogInitial);
+    var propertyPath = FileUtils.getUniquePath(
+      FileUtils.replaceLastSegmentInPath(modelPath, FTS4VMCAnalysis.PROPERTY_FILE), false, false);
+    Files.writeString(Paths.get(propertyPath), property);
+    // run model checker and process results
+    var result = runFTS4VMC(modelPath, propertyPath);
+    var resultPath = FileUtils.getUniquePath(
+      FileUtils.replaceLastSegmentInPath(modelPath, FTS4VMCAnalysis.SAT_FILE), false, false);
+    Files.writeString(Paths.get(resultPath), result);
+    var filesCtx = templateElems.get("filesCtx");
+    var filesDesc = filesCtx.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+      .replace("{property}", FileUtils.getLastSegmentFromPath(propertyPath))
+      .replace("{model}", FileUtils.getLastSegmentFromPath(modelPath));
+    filesCtx.setAttribute(this.gsn.getArgumentElement_Description(), filesDesc);
+    filesCtx.setManyAttribute(GSNTemplatesPackage.eINSTANCE.getFilesContext_Paths(),
+                              ECollections.asEList(List.of(propertyPath, modelPath, resultPath)));
+    var satGoal = templateElems.get("satGoal");
+    var satGoalDesc = satGoal.getAttribute(this.gsn.getArgumentElement_Description()).get(0);
+    var holds = (result.contains("TRUE")) ? "holds" : "does not hold";
+    satGoal.setAttribute(this.gsn.getArgumentElement_Description(), satGoalDesc.replace("{holds?}", holds));
+    var satSolution = templateElems.get("satSolution");
+    var satSolDesc = satSolution.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+      .replace("{output}", FileUtils.getLastSegmentFromPath(resultPath));
+    satSolution.setAttribute(this.gsn.getArgumentElement_Description(), satSolDesc);
+    if (!safetyGoal.isAlwaysPresent()) {
+      var pc = safetyGoal.getPresenceCondition();
+      plTemplate.getStreamOfReference(this.gsn.getTemplate_Elements()).forEach(e -> {
+        e.setPresenceCondition(pc);
+        e.getStreamOfReference(this.gsn.getSupportable_SupportedBy()).forEach(sb -> sb.setPresenceCondition(pc));
+        e.getStreamOfReference(this.gsn.getContextualizable_InContextOf()).forEach(ico -> ico.setPresenceCondition(pc));
+      });
+    }
+  }
+
+  @Override
+  public List<PLGSNChangeStep> nextImpactSteps(PLGSNAnalyticTemplate plTemplate, PLGSNChangeStep step)
+                                              throws Exception {
+    // leaf template
+    return List.of();
+  }
+
+  @Override
+  public void impact(PLGSNAnalyticTemplate plTemplate, PLGSNChangeStep step) throws Exception {
+    var templateElems = plTemplate.getElementsById();
+    var safetyGoal = templateElems.get("safetyGoal");
+    if (safetyGoal == null) {
+      // using the forward trace to get safetyGoal does not work with linked templates, so navigate the gsn tree
+      safetyGoal = (PLGSNArgumentElement) templateElems.get("mcStrategy")
+        .getReference(this.gsn.getSupporter_Supports()).get(0)
+        .getReference(this.gsn.getSupportedBy_Source()).get(0);
+    }
+    var prevImpact = safetyGoal.getImpact();
+    var satGoal = templateElems.get("satGoal");
+    var satSolution = templateElems.get("satSolution");
+    Map<ImpactType, Optional<String>> impactType;
+    if (prevImpact.get(ImpactType.REVISE).isPresent()) {
+      // parents should be revised top down, do not re-run model checking but mark to revise
+      impactType = PLGSNChangeStep.REVISE;
+    }
+    else if ((boolean) ChangeStep.getData().get(ChangeStep.RUN_EVIDENCE_ANALYSES_KEY)) {
+      // re-run model checking
+      var filesCtx = templateElems.get("filesCtx");
+      var paths = filesCtx.getManyAttribute(GSNTemplatesPackage.eINSTANCE.getFilesContext_Paths()).get(0);
+      var propertyPath = paths.get(0);
+      var modelPath = paths.get(1);
+      var resultPath = paths.get(2);
+      var result = runFTS4VMC(modelPath, propertyPath);
+      var propsKey = getClass().getName() + "_" + modelPath + "_" + propertyPath + "_" + resultPath;
+      ChangeStep.getData().put(propsKey, result);
+      var holds = result.contains("TRUE");
+      var oldHolds = satGoal.getAttribute(this.gsn.getArgumentElement_Description()).get(0).contains("holds");
+      impactType = (holds == oldHolds) ? PLGSNChangeStep.REUSE : PLGSNChangeStep.REVISE;
+    }
+    else {
+      var templateStep = new PLGSNChangeStep(satSolution);
+      templateStep.getBackwardTrace().add(List.of());
+      templateStep.baselineImpact();
+      impactType = satSolution.getImpact();
+    }
+    satGoal.setImpact(impactType);
+    satSolution.setImpact(impactType);
+    safetyGoal.setImpact(impactType);
+    // reuse everything else in the template
+    PLGSNChangeStep.setAllImpacts(plTemplate, PLGSNChangeStep.REUSE);
+  }
+
+  @Override
+  public List<PLGSNChangeStep> repair(PLGSNAnalyticTemplate plTemplate, PLGSNChangeStep step) throws Exception {
+    return IPLGSNAnalysis.super.repair(plTemplate, step);
+    //TODO nothing to be done, apart from updating holds/does not hold and storing result
+  }
+}
--- examples/FAC25/edu.toronto.cs.se.mmint.examples.fac25/FAC25/src/fac25/VQLQueryAnalysis.java
+++ examples/FAC25/edu.toronto.cs.se.mmint.examples.fac25/FAC25/src/fac25/VQLQueryAnalysis.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025, 2025 Alessio Di Sandro.
+ * Copyright (c) 2024, 2025 Alessio Di Sandro.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -17,24 +17,201 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.common.util.ECollections;
 import org.eclipse.emf.ecore.EObject;
 
 import edu.toronto.cs.se.mmint.MMINT;
+import edu.toronto.cs.se.mmint.mid.diagram.library.SiriusEvaluateQuery;
+import edu.toronto.cs.se.mmint.mid.diagram.library.SiriusEvaluateQuery.ResultPrinter;
 import edu.toronto.cs.se.mmint.mid.reasoning.IQueryTrait;
 import edu.toronto.cs.se.mmint.mid.reasoning.IQueryTrait.QuerySpec;
+import edu.toronto.cs.se.mmint.mid.ui.MIDDialogs;
 import edu.toronto.cs.se.mmint.mid.utils.FileUtils;
+import edu.toronto.cs.se.mmint.productline.Class;
+import edu.toronto.cs.se.mmint.productline.PLElement;
 import edu.toronto.cs.se.mmint.productline.ProductLine;
 import edu.toronto.cs.se.mmint.types.gsn.productline.PLGSNAnalyticTemplate;
 import edu.toronto.cs.se.mmint.types.gsn.productline.PLGSNArgumentElement;
+import edu.toronto.cs.se.mmint.types.gsn.productline.reasoning.IPLGSNAnalysis;
 import edu.toronto.cs.se.mmint.types.gsn.productline.util.PLGSNBuilder;
 import edu.toronto.cs.se.mmint.types.gsn.productline.util.PLGSNChangeStep;
+import edu.toronto.cs.se.mmint.types.gsn.templates.AnalyticTemplate;
+import edu.toronto.cs.se.mmint.types.gsn.templates.FilesContext;
 import edu.toronto.cs.se.mmint.types.gsn.templates.GSNTemplatesPackage;
+import edu.toronto.cs.se.mmint.types.gsn.templates.util.GSNTemplatesBuilder;
+import edu.toronto.cs.se.modelepedia.gsn.Context;
+import edu.toronto.cs.se.modelepedia.gsn.GSNPackage;
+import edu.toronto.cs.se.modelepedia.gsn.Goal;
 import edu.toronto.cs.se.modelepedia.gsn.ImpactType;
+import edu.toronto.cs.se.modelepedia.gsn.SafetyCase;
+import edu.toronto.cs.se.modelepedia.gsn.Strategy;
 import edu.toronto.cs.se.modelepedia.gsn.util.ChangeStep;
 
-public class VQLQueryAnalysis2 extends VQLQueryAnalysis {
+/**
+ * This template is a decomposition strategy based on identifying certain patterns in the underlying model with a VQL
+ * query. It does not generate any evidence, but is supported by downstream goals corresponding to each of the
+ * identified patterns.
+ */
+public class VQLQueryAnalysis implements IPLGSNAnalysis {
+  protected GSNPackage gsn;
+
+  public VQLQueryAnalysis() {
+    this.gsn = GSNPackage.eINSTANCE;
+  }
+
+  public static ResultPrinter PL_NAME_PRINTER = (result) -> {
+    if (result instanceof Class plClass) {
+      // try finding a name
+      var name = plClass.getAttributes().stream()
+        .filter(a -> a.getType().getName().equals("name"))
+        .map(a -> a.getValue())
+        .collect(Collectors.joining(", "));
+      if (!name.isEmpty()) {
+        return name;
+      }
+    }
+    return SiriusEvaluateQuery.NAME_PRINTER.prettyPrint(result);
+  };
+
+  protected List<String> getResults(List<Object> queryResults) {
+    return queryResults.stream()
+      .map(r -> SiriusEvaluateQuery.queryResultToString(r, SiriusEvaluateQuery.NAME_PRINTER, null, null))
+      .filter(r -> r.startsWith("Alrm_"))
+      .sorted() // for reproducibility
+      .collect(Collectors.toList());
+  }
+
+  @Override
+  public void instantiate(AnalyticTemplate template) throws Exception {
+    var safetyCase = (SafetyCase) template.eContainer();
+    var builder = new GSNTemplatesBuilder(safetyCase);
+    // get template elems
+    var templateElems = template.getElementsById();
+    var safetyGoal = (Goal) templateElems.get("safetyGoal");
+    var filesCtx = (FilesContext) templateElems.get("filesCtx");
+    var scenarioGoal = (Goal) templateElems.get("scenarioGoal");
+    var resultStrategy = (Strategy) templateElems.get("resultStrategy");
+    var resultCtx = (Context) templateElems.get("resultCtx");
+    var resultGoal = (Goal) templateElems.get("resultGoal");
+    safetyGoal.instantiate();
+    var safetyDesc = safetyGoal.getDescription();
+    var resultId = resultGoal.getId();
+    var resultDesc = resultGoal.getDescription().replace("{safety goal}", safetyDesc);
+    scenarioGoal.setDescription(scenarioGoal.getDescription().replace("{safety goal}", safetyDesc));
+    // run query and process results
+    var modelPath = FileUtils.prependWorkspacePath(MIDDialogs.selectModelToImport(false));
+    var rootModelObj = FileUtils.readModelFile(modelPath, null, false);
+    var querySpec = SiriusEvaluateQuery.selectQuery(rootModelObj);
+    var queryResults = querySpec.evaluateQuery(rootModelObj, List.of());
+    template.getElements().remove(resultGoal);
+    safetyCase.getGoals().remove(resultGoal);
+    resultStrategy.getSupportedBy().remove(0);
+    filesCtx.setDescription(
+      filesCtx.getDescription()
+              .replace("{query}", querySpec.query().toString())
+              .replace("{model}", FileUtils.getLastSegmentFromPath(modelPath)));
+    filesCtx.getPaths().add(querySpec.filePath());
+    filesCtx.getPaths().add(modelPath);
+    var results = getResults(queryResults);
+    var resultCtxDesc = (results.isEmpty()) ? "No results" : "Query results:";
+    var i = 0;
+    for (var result : results) {
+      resultCtxDesc += "\n'" + result + "'";
+      resultGoal = builder.createGoal(resultId.replace("X", String.valueOf(i)),
+                                      resultDesc.replace("{X}", "'" + result + "'"));
+      resultGoal.setTemplateId("resultGoal" + i);
+      template.getElements().add(resultGoal);
+      builder.addSupporter(resultStrategy, resultGoal);
+      i++;
+    }
+    resultCtx.setDescription(resultCtxDesc);
+  }
+
+  @Override
+  public void import_(PLGSNAnalyticTemplate plTemplate, ProductLine productLine) throws Exception {
+    var plBuilder = new PLGSNBuilder(productLine);
+    var queryStrategy = plTemplate.getElementsById().get("queryStrategy");
+    var desc = queryStrategy.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+      .replace("model query", "lifted model query");
+    queryStrategy.setAttribute(this.gsn.getArgumentElement_Description(), desc);
+    var liftedGoal = plBuilder.createGoal("G4", "The lifted query engine is correct", null);
+    liftedGoal.addAttribute(this.gsn.getArgumentElement_TemplateId(), "liftedGoal");
+    plTemplate.addReference(this.gsn.getTemplate_Elements(), liftedGoal);
+    plBuilder.addSupporter(queryStrategy, liftedGoal);
+  }
+
+  protected List<Map.Entry<String, String>> getPLResults(List<Object> queryResults) {
+    return queryResults.stream()
+      .map(r -> Map.entry(
+        SiriusEvaluateQuery.queryResultToString(r, VQLQueryAnalysis.PL_NAME_PRINTER, null, null),
+        (r instanceof PLElement plResult) ? plResult.getPresenceCondition() : null))
+      .filter(r -> r.getKey().startsWith("Alrm_"))
+      .sorted(Map.Entry.comparingByKey()) // for reproducibility
+      .collect(Collectors.toList());
+  }
+
+  protected PLGSNArgumentElement createPLResultGoal(PLGSNAnalyticTemplate plTemplate, PLGSNBuilder plBuilder,
+                                                    PLGSNArgumentElement resultStrategy, String resultId,
+                                                    String resultDesc, String resultPC, String templateId) {
+    var resultGoal = plBuilder.createGoal(resultId, resultDesc, resultPC);
+    resultGoal.addAttribute(this.gsn.getArgumentElement_TemplateId(), templateId);
+    plTemplate.addReference(this.gsn.getTemplate_Elements(), resultGoal);
+    plBuilder.addSupporter(resultStrategy, resultGoal);
+
+    return resultGoal;
+  }
+
+  @Override
+  public void instantiate(PLGSNAnalyticTemplate plTemplate) throws Exception {
+    var productLine = (ProductLine) plTemplate.eContainer();
+    var plBuilder = new PLGSNBuilder(productLine);
+    // get template elems
+    var templateElems = plTemplate.getElementsById();
+    var safetyGoal = templateElems.get("safetyGoal");
+    var filesCtx = templateElems.get("filesCtx");
+    var scenarioGoal = templateElems.get("scenarioGoal");
+    var resultStrategy = templateElems.get("resultStrategy");
+    var resultCtx = templateElems.get("resultCtx");
+    var resultGoal = templateElems.get("resultGoal");
+    safetyGoal.instantiate();
+    var safetyDesc = safetyGoal.getAttribute(this.gsn.getArgumentElement_Description()).get(0);
+    var resultId = resultGoal.getAttribute(this.gsn.getArgumentElement_Id()).get(0);
+    var resultDesc = resultGoal.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+      .replace("{safety goal}", safetyDesc);
+    var scenarioDesc = scenarioGoal.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+                                   .replace("{safety goal}", safetyDesc);
+    scenarioGoal.setAttribute(this.gsn.getArgumentElement_Description(), scenarioDesc);
+    // run query and process results
+    var modelPath = FileUtils.prependWorkspacePath(
+      MIDDialogs.selectFile("Run Product Line analysis", "Select a Product Line model",
+                            "There are no Product Line models in the workspace", Set.of("productline")));
+    var rootModelObj = FileUtils.readModelFile(modelPath, null, false);
+    var querySpec = SiriusEvaluateQuery.selectQuery(rootModelObj);
+    var queryResults = querySpec.evaluateQuery(rootModelObj, List.of());
+    resultGoal.delete();
+    for (var supportedBy : resultGoal.getReference(this.gsn.getSupporter_Supports())) {
+      supportedBy.delete();
+    }
+    var filesDesc = filesCtx.getAttribute(this.gsn.getArgumentElement_Description()).get(0)
+                            .replace("{query}", querySpec.query().toString())
+                            .replace("{model}", FileUtils.getLastSegmentFromPath(modelPath));
+    filesCtx.setAttribute(this.gsn.getArgumentElement_Description(), filesDesc);
+    filesCtx.setManyAttribute(GSNTemplatesPackage.eINSTANCE.getFilesContext_Paths(),
+                              ECollections.asEList(List.of(querySpec.filePath(), modelPath)));
+    var results = getPLResults(queryResults);
+    var resultCtxDesc = (results.isEmpty()) ? "No results" : "Query results:";
+    var i = 0;
+    for (var result : results) {
+      resultCtxDesc += "\n'" + result.getKey() + "'";
+      createPLResultGoal(plTemplate, plBuilder, resultStrategy, resultId.replace("X", String.valueOf(i)),
+                         resultDesc.replace("{X}", "'" + result.getKey() + "'"), result.getValue(), "resultGoal" + i);
+      i++;
+    }
+    resultCtx.setAttribute(this.gsn.getArgumentElement_Description(), resultCtxDesc);
+  }
 
   @Override
   public List<PLGSNChangeStep> nextImpactSteps(PLGSNAnalyticTemplate plTemplate, PLGSNChangeStep step)
