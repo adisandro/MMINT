8405b9e1b5683ccf994099558eb19f775ad158ce
Alessio Di Sandro <alessio.disandro@gmail.com>
2026-01-06 21:28:27 +0100

    releng: Bump copyright to 2026

--- plugins/Types/ClassDiagram/edu.toronto.cs.se.modelepedia.classdiagram.design/src/edu/toronto/cs/se/modelepedia/classdiagram/design/Services.java
+++ plugins/Types/ClassDiagram/edu.toronto.cs.se.modelepedia.classdiagram.design/src/edu/toronto/cs/se/modelepedia/classdiagram/design/Services.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012-2026 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
 package edu.toronto.cs.se.modelepedia.classdiagram.design;
 
 import org.eclipse.emf.ecore.EObject;
@@ -6,7 +17,7 @@ import org.eclipse.emf.ecore.EObject;
  * The services class used by VSM.
  */
 public class Services {
-    
+
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
--- plugins/Types/SequenceDiagram/edu.toronto.cs.se.modelepedia.sequencediagram.design/src/edu/toronto/cs/se/modelepedia/sequencediagram/design/Services.java
+++ plugins/Types/SequenceDiagram/edu.toronto.cs.se.modelepedia.sequencediagram.design/src/edu/toronto/cs/se/modelepedia/sequencediagram/design/Services.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012-2026 Marsha Chechik, Alessio Di Sandro, Michalis Famelis,
+ * Rick Salay.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alessio Di Sandro - Implementation.
+ */
 package edu.toronto.cs.se.modelepedia.sequencediagram.design;
 
 import org.eclipse.emf.ecore.EObject;
@@ -8,7 +19,7 @@ import edu.toronto.cs.se.modelepedia.sequencediagram.SequenceDiagram;
  * The services class used by VSM.
  */
 public class Services {
-    
+
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
@@ -16,13 +27,13 @@ public class Services {
        // TODO Auto-generated code
       return self;
     }
-    
+
     public EObject getSequenceDiagram(EObject self) {
-    	EObject curObject = self;
+    	var curObject = self;
     	while (!(curObject == null || curObject instanceof SequenceDiagram)) {
     		curObject = curObject.eContainer();
     	}
-    	
+
     	return curObject;
     }
 }
